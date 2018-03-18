dcb4d4247720b4c87c6e953b33f8e30f7b8f54b0
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-17 21:44:03 -0400

    database icon

--- navbar.php
+++ navbar.php
@@ -23,7 +23,7 @@
 
                     <?php echo (basename($_SERVER['PHP_SELF']) == "storage-pools.php") ? '<li class="active">' : '<li>'; ?>
                         <a href="storage.php">
-                            <i class="fas f1c0"></i>
+                            <i class="now-ui-icons files_box"></i>
                             <p>Storage</p>
                         </a>
                     </li>
