906e3d7a2a3eae3012669c29e38931fcbbaa4ae6
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-21 09:36:58 -0400

    commit

--- guest-single.php
+++ guest-single.php
@@ -378,10 +378,10 @@ echo "<br />";
           echo "<h3>Domain XML</h3>";
           $inactive = (!$lv->domain_is_running($domName)) ? true : false;
           $xml = $lv->domain_get_xml($domName, $inactive);
-          $ret = "Domain XML for domain <i>$domName</i>:<br/><br/>".htmlentities($xml);
-          echo "XML description: <table><tr><td>Domain XML: </td>".
+          $ret = htmlentities($xml);
+          echo "<table><tr>".
             "<td><textarea name=\"xmldesc\" rows=\"25\" cols=\"90%\">".$ret."</textarea></t\></tr><tr align=\"center\"><td colspan=\"2\">".
-            "</tr>";
+            "</tr></table>";
 
           if ($action == 'domain-edit') {
             if (@$_POST['xmldesc']) {
