536901ad9e5c799fcbe60e0cf40affa82b594c7c
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-21 10:03:48 -0400

    commit

--- guest-single.php
+++ guest-single.php
@@ -402,7 +402,7 @@ echo "<br />";
                   $inactive = (!$lv->domain_is_running($domName)) ? true : false;
                   $xml = $lv->domain_get_xml($domName, $inactive);
                   $ret = htmlentities($xml);
-                  echo "<textarea rows=\"25\" cols=\"90%\" boarder='0px' readonly>" . $ret . "</textarea>";
+                  echo "<textarea rows=\"15\" cols=\"90%\" readonly>" . $ret . "</textarea>";
                   ?>
                 </div>
                 <div class="tab-pane" id="linkxmledit">
@@ -423,7 +423,7 @@ echo "<br />";
             </div>
           </div>
 
-        
+        </div>
 
 
       </div>
