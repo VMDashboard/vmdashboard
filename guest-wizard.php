7922854b53d7591fa8a89113498808b557eb5c7f
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-22 06:04:48 -0400

    testing

--- guest-wizard.php
+++ guest-wizard.php
@@ -135,8 +135,8 @@ $xml = "
 
 $ret = $lv->domain_define($xml) ? "Domain successfully added" : "Cannot add domain: ".$lv->get_last_error();
 echo $ret;
-//header('Location: guests.php');
-//exit;
+header('Location: guests.php');
+exit;
 }
 ?>
 
@@ -160,10 +160,6 @@ $random_mac = $lv->generate_random_mac_addr();
 
           <div class="card-header text-center" data-background-color="orange">
             <h3 class="card-title">
-              <?php var_dump($new_disk); ?>
-              <br>
-              <?php echo $new_disk; ?>
-              <br>
               Create a new guest VM
             </h3>
 
