c79a43e8e2baebe47d367b0e7756c298c0b4a3c9
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-22 23:06:19 -0400

    commit

--- guest-single.php
+++ guest-single.php
@@ -187,49 +187,6 @@ require('navbar.php');
                 </a>
               <?php } ?>
 
-<script>
-$('a').click(function(e){
-    e.preventDefault();
-    var link = $(this).attr('href');
-
-    swal({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      type: 'warning',
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!',
-      cancelButtonText: 'No, cancel!',
-      confirmButtonClass: 'btn btn-success',
-      cancelButtonClass: 'btn btn-danger',
-      buttonsStyling: false,
-      reverseButtons: true
-    }).then((result) => {
-      if (result.value) {
-        swal(
-          'Deleted!',
-          'Your file has been deleted.',
-          'success'
-        )
-      } else if (
-        // Read more about handling dismissals
-        result.dismiss === swal.DismissReason.cancel
-      ) {
-        swal(
-          'Cancelled',
-          'Your imaginary file is safe :)',
-          'error'
-        )
-      }
-    })
-
-
-
-});
-</script>
-
-
 
             </div>
 
