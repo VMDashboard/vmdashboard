2479dd534bdb5e5a5b8ea3b0ea838e57010d2e54
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-23 06:00:37 -0400

    test

--- guest-single.php
+++ guest-single.php
@@ -286,7 +286,7 @@ linkURL = window.location.href + linkURL;
                 "<td>$allocation</td>" .
                 "<td>$physical</td>" .
                 "<td>" .
-                  "<a title='Remove disk device' onclick=\"diskRemoveWarning('&amp;action=domain-disk-remove&amp;dev=" . $tmp[$i]['device'] ."')\" href='#'><i class='fas fa-trash-alt'></i></a>" .
+                  "<a title='Remove disk device' onclick=\"diskRemoveWarning('&amp;action=domain-disk-remove&amp;dev=" . $tmp[$i]['device'] . "')\" href='#'><i class='fas fa-trash-alt'></i></a>" .
                 "</td>" .
                 "</tr>";
             }
