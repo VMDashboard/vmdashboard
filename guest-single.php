1a659d267d2546a3888882c27d66a406ef29776b
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-22 23:05:28 -0400

    commit

--- guest-single.php
+++ guest-single.php
@@ -193,14 +193,39 @@ $('a').click(function(e){
     var link = $(this).attr('href');
 
     swal({
-        title: "Are you sure?",
-        text: "By clicking 'OK' you will be redirected to the link.",
-        type: "warning",
-        showCancelButton: true
-    },
-    function(){
-        window.location.href = link;
-    });
+      title: 'Are you sure?',
+      text: "You won't be able to revert this!",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, cancel!',
+      confirmButtonClass: 'btn btn-success',
+      cancelButtonClass: 'btn btn-danger',
+      buttonsStyling: false,
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        swal(
+          'Deleted!',
+          'Your file has been deleted.',
+          'success'
+        )
+      } else if (
+        // Read more about handling dismissals
+        result.dismiss === swal.DismissReason.cancel
+      ) {
+        swal(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
+
+
+
 });
 </script>
 
