f80774e576ce3709dcb2ff6f311c892d055ef836
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-22 23:21:31 -0400

    sweet alert

--- guest-single.php
+++ guest-single.php
@@ -196,7 +196,7 @@ function warnBeforeRedirect(linkURL) {
                  showCancelButton: true
                }, function() {
                  // Redirect the user
-                 window.location.href = linkURL;
+                 window.location.href = window.location.href + linkURL;
                });
              }
 </script>
