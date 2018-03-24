5afaa63b62c2c0ab503b1966f3aa5e85bc6e8a23
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-24 19:22:45 -0400

    test

--- storage.php
+++ storage.php
@@ -11,7 +11,7 @@ require('navbar.php');
       <div class="card">
         <div class="card-header">
           <h4 class="card-title">Storage Pools</h4>
-          <button onclick="window.location.href='storage.php" class="btn btn-primary btn-round btn-simple">Create new storage pool </button>
+          <a href="storage.php">Create new storage pool </a>
         </div>
         <div class="card-body">
 
