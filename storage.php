9ba45fed38aca07bff28c3af23b761eff02f58d1
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-24 11:55:54 -0400

    test

--- storage.php
+++ storage.php
@@ -7,20 +7,21 @@ require('navbar.php');
 </div>
 
 <div class="content">
+
+  <?php
+  $pools = $lv->get_storagepools();
+  for ($i = 0; $i < sizeof($pools); $i++) {
+    $info = $lv->get_storagepool_info($pools[$i]);
+  ?>
+
   <div class="row">
     <div class="col-md-12">
       <div class="card">
         <div class="card-header">
-          <h4 class="card-title"> Storage</h4>
+          <h4 class="card-title"> <?php echo $pools[$i] . " Storage Pool"; ?></h4>
         </div>
-
-        <?php
-        $pools = $lv->get_storagepools();
-        for ($i = 0; $i < sizeof($pools); $i++) {
-          $info = $lv->get_storagepool_info($pools[$i]);
-          echo {$pools[$i]} . " Storage Pool";
-        ?>
         <div class="card-body">
+
           <div class="table-responsive">
             <table class="table">
               <thead class=" text-primary">
