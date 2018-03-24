d75a65d8995f9307090a852aef4e74c632ea0283
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-24 13:56:39 -0400

    test

--- storage.php
+++ storage.php
@@ -104,7 +104,7 @@ require('navbar.php');
           <div class="row">
             <div class="col-md-3">
             <h4><font style="color:#f96332;"><?php echo $pools[$i]; ?></font></h4>
-            <button onclick="window.location.href='storage-volume-wizard.php'" class="btn btn-primary btn-round btn-simple">Create new volume </button>
+            <button onclick="window.location.href='storage-volume-wizard.php?action=storage-pools&amp;pool=<?php echo $pools[$i]; ?>&amp;subaction=volume-create'" class="btn btn-primary btn-round btn-simple">Create new volume </button>
             <br><a href="storage-volume-wizard.php?action=storage-pools&amp;pool=<?php echo $pools[$i]; ?>&amp;subaction=volume-create">Create new volume</a><br>
           <?php $act = $info['active'] ? 'Active' : 'Inactive';
           echo "<strong>Pool Name:</strong> " . $pools[$i] . "<br />";
