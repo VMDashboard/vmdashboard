21a466f95cc35c337d8f55a26fd40f4e00befea5
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-24 20:31:16 -0400

    commit

--- storage.php
+++ storage.php
@@ -97,7 +97,7 @@ require('navbar.php');
           <hr>
           <div class="row">
             <div class="col-md-3">
-            <font style="font-size:24px;"><?php echo $pools[$i]; ?></font><hr />
+            <font style="font-size:24px;"><?php echo $pools[$i]; ?></font><br />
             <a href="storage-volume-wizard.php?action=storage-pools&amp;pool=<?php echo $pools[$i]; ?>&amp;subaction=volume-create"><i class="fas fa-plus"></i> Create new volume </a>
             <br /><br/>
           <?php $act = $info['active'] ? 'Active' : 'Inactive';
