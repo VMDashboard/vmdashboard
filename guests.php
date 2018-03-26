252fb6a7692479ac7e32bc2cfd9ba7a363971caf
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-03-26 06:20:20 -0400

    commit

--- guests.php
+++ guests.php
@@ -57,65 +57,63 @@ swal(alertRet);
           <a href="guest-wizard.php"><i class="fas fa-plus"></i> Create new guest </a>
         </div>
         <div class="card-body">
-        <div class="table-responsive">
-          <table class="table">
-            <thead class=" text-primary">
-              <th>Name</th>
-              <th>CPU Cores</th>
-              <th>Memory</th>
-              <th>Disk(s)</th>
-              <th>Power State</th>
-              <th>Action</th>
-            </thead>
-            <tbody>
-<?php
-$doms = $lv->get_domains();
-foreach ($doms as $name) {
-  $dom = $lv->get_domain_object($name);
-  $uuid = libvirt_domain_get_uuid_string($dom);
-  $active = $lv->domain_is_active($dom);
-  $info = $lv->domain_get_info($dom);
-  $mem = number_format($info['memory'] / 1024, 0, '.', '').' MB';
-  $cpu = $info['nrVirtCpu'];
-  $state = $lv->domain_state_translate($info['state']);
-  $id = $lv->domain_get_id($dom);
-  $arch = $lv->domain_get_arch($dom);
-  $vnc = $lv->domain_get_vnc_port($dom);
-  $nics = $lv->get_network_cards($dom);
+          <div class="table-responsive">
+            <table class="table">
+              <thead class=" text-primary">
+                <th>Name</th>
+                <th>CPU Cores</th>
+                <th>Memory</th>
+                <th>Disk(s)</th>
+                <th>Power State</th>
+                <th>Action</th>
+              </thead>
+              <tbody>
+                <?php
+                $doms = $lv->get_domains();
+                foreach ($doms as $name) {
+                  $dom = $lv->get_domain_object($name);
+                  $uuid = libvirt_domain_get_uuid_string($dom);
+                  $active = $lv->domain_is_active($dom);
+                  $info = $lv->domain_get_info($dom);
+                  $mem = number_format($info['memory'] / 1024, 0, '.', '').' MB';
+                  $cpu = $info['nrVirtCpu'];
+                  $state = $lv->domain_state_translate($info['state']);
+                  $id = $lv->domain_get_id($dom);
+                  $arch = $lv->domain_get_arch($dom);
+                  $vnc = $lv->domain_get_vnc_port($dom);
+                  $nics = $lv->get_network_cards($dom);
 
-	if (($diskcnt = $lv->get_disk_count($dom)) > 0) {
-        $disks = $diskcnt.' / '.$lv->get_disk_capacity($dom);
-        $diskdesc = 'Current physical size: '.$lv->get_disk_capacity($dom, true);
-    } else {
-        $disks = '-';
-        $diskdesc = '';
-    }
+	                 if (($diskcnt = $lv->get_disk_count($dom)) > 0) {
+                     $disks = $diskcnt.' / '.$lv->get_disk_capacity($dom);
+                     $diskdesc = 'Current physical size: '.$lv->get_disk_capacity($dom, true);
+                   } else {
+                     $disks = '-';
+                     $diskdesc = '';
+                   }
 
-    unset($tmp);
-    unset($dom);
+                   unset($tmp);
+                   unset($dom);
 
-	echo "<tr>" .
-    "<td>" .
-    "<a href=\"guest-single.php?uuid=$uuid\">$name</a>" .
-    "</td>" .
-    "<td>$cpu</td>" .
-    "<td>$mem</td>" .
-    "<td title='$diskdesc'>$disks</td>" .
-    "<td>$state</td>";
+	                 echo "<tr>" .
+                    "<td>" .
+                    "<a href=\"guest-single.php?uuid=$uuid\">$name</a>" .
+                    "</td>" .
+                    "<td>$cpu</td>" .
+                    "<td>$mem</td>" .
+                    "<td title='$diskdesc'>$disks</td>" .
+                    "<td>$state</td>";
+                   echo "<td>";
 
-    echo "<td>";
-
-	if ($lv->domain_is_running($name)){
-    echo "<a href=\"?action=domain-stop&amp;uuid=$uuid\">Shutdown</a> | <a href=\"?action=domain-destroy&amp;uuid=$uuid\">Turn off</a> | <a href=\"?action=domain-pause&amp;uuid=$uuid\">Pause</a>";
-  } else if ($lv->domain_is_paused($name)){
-    echo "<a href=\"?action=domain-resume&amp;uuid=$uuid\">Resume</a>";
-  } else
-    echo "<a href=\"?action=domain-start&amp;uuid=$uuid\"><i class='fas fa-power-off'></i> Power on</a>";
-    echo "</td>" ;
-	  echo "</tr>";
-}
-?>
-
+	                 if ($lv->domain_is_running($name)){
+                     echo "<a href=\"?action=domain-stop&amp;uuid=$uuid\">Shutdown</a> | <a href=\"?action=domain-destroy&amp;uuid=$uuid\">Turn off</a> | <a href=\"?action=domain-pause&amp;uuid=$uuid\">Pause</a>";
+                   } else if ($lv->domain_is_paused($name)){
+                     echo "<a href=\"?action=domain-resume&amp;uuid=$uuid\">Resume</a>";
+                   } else
+                   echo "<a href=\"?action=domain-start&amp;uuid=$uuid\"><i class='fas fa-power-off'></i> Power on</a>";
+                   echo "</td>" ;
+	                  echo "</tr>";
+                  }
+                  ?>
 
               </tbody>
             </table>
