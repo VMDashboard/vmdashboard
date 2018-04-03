de617ad16fd1ad22caf907e32a9c5852f1aad05b
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-02 21:23:19 -0400

    test

--- guest-single.php
+++ guest-single.php
@@ -45,7 +45,7 @@ if ($action == 'domain-delete') {
 //Disk actions
 if ($action == 'domain-disk-remove') {
   //$ret = $lv->domain_disk_remove($domName, $_GET['dev']) ? 'Disk has been removed successfully' : 'Cannot remove disk: '.$lv->get_last_error();
-  $ret = $lv->domain_disk_remove($domName, "newVM4.qcow2") ? 'Disk has been removed successfully' : 'Cannot remove disk: '.$lv->get_last_error();
+  $ret = $lv->domain_disk_remove($domName, "/var/lib/libvirt/images/newVM4.qcow2") ? 'Disk has been removed successfully' : 'Cannot remove disk: '.$lv->get_last_error();
 
 }
 
