fa611af6a94c8b2a19c562680613494e89ebe53f
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-04 05:52:54 -0400

    commit

--- guest-single.php
+++ guest-single.php
@@ -436,7 +436,7 @@ $list = file_put_contents($listfile, $liststring);
                   <?php
                   if ($state == "shutoff"){
                     $ret = "<form method=\"POST\" action=?action=domain-edit&amp;uuid=" . $_GET['uuid'] . " >" .
-                      "<textarea style=\"background:black; color:white;\" name=\"xmldesc\" rows=\"17\" cols=\"2\" style=\"width: 100%; margin: 0; padding: 0; border-width: 0;\" >" . $xml . "</textarea>" .
+                      "<textarea style=\"background-color:black; color:white;\" name=\"xmldesc\" rows=\"17\" cols=\"2\" style=\"width: 100%; margin: 0; padding: 0; border-width: 0;\" >" . $xml . "</textarea>" .
                       "<br /> <br /> <input type=\"submit\" value=\"Save XML\"></form>";
                     echo $ret;
                   } else {
