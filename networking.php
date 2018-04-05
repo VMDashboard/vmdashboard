31ab96e1af97766d3a113308f97ce60d61c8830c
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-04 22:47:56 -0400

    commit

--- networking.php
+++ networking.php
@@ -1,5 +1,8 @@
 <?php
 require('header.php');
+if ($action == "delete-network") {
+
+}
 require('navbar.php');
 ?>
 
@@ -84,7 +87,7 @@ for ($i = 0; $i < sizeof($tmp); $i++) {
 
   if (!$tmp2['active']) {
     $act .= ' | <a href="?action='.$_GET['action'].'&amp;subaction=edit&amp;name='. urlencode($tmp2['name']) . '">Edit network</a>';
-    $act .= ' | <a href="?action=delete-network">Delete</a>';
+    $act .= ' | <a href="?action=delete-network&amp;network='.$tmp2['name'].'">Delete</a>';
   }
   echo "<tr>" .
     "<td>{$tmp2['name']}</td>" .
