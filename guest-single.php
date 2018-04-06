45f8285929fa2e4afe85274de275cf7ec1c53704
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-05 21:37:27 -0400

    commit

--- guest-single.php
+++ guest-single.php
@@ -289,7 +289,7 @@ $list = file_put_contents($listfile, $liststring);
           <?php
           /* Network interface information */
           echo "<h3>Network devices</h3>";
-          echo "<a href=\"guest-network-wizard?uuid=$uuid\">Add new network interface</a>";
+          echo "<a href=\"guest-network-wizard.php?uuid=$uuid\">Add new network interface</a>";
           $tmp = $lv->get_nic_info($domName);
           if (!empty($tmp)) {
             $anets = $lv->get_networks(VIR_NETWORKS_ACTIVE);
