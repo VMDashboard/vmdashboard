42506aeb4181594f61a0bae827ff35cb390a1676
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-05 20:36:47 -0400

    commit

--- guest-network-wizard.php
+++ guest-network-wizard.php
@@ -20,12 +20,13 @@ if (isset($_POST['finish'])) {
   $network = $_POST['network'];
   $model = $_POST['model'];
 
-  $ret = $lv->domain_nic_add($domName, $mac, $network, $model) ? "Network has been successfully added to the guest" : "Cannot add network to the guest: ".$lv->get_last_error();
+  $ret = $lv->domain_nic_add($domName, $mac, $network, $model) ? "success" : "Cannot add network to the guest: ".$lv->get_last_error();
 
-
+  if ($ret == "success"){
   //Return back to the orignal web page
   header('Location: ' . "guest-single.php?uuid=$uuid");
   exit;
+  }
 }
 
 require('navbar.php');
