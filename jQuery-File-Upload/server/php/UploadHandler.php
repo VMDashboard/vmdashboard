572411f41fb50bf3db4958be579f9e33cab24764
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-08 16:38:08 -0400

    commit

--- jQuery-File-Upload/server/php/UploadHandler.php
+++ jQuery-File-Upload/server/php/UploadHandler.php
@@ -44,8 +44,8 @@ class UploadHandler
         $this->response = array();
         $this->options = array(
             'script_url' => $this->get_full_url().'/'.$this->basename($this->get_server_var('SCRIPT_NAME')),
-            'upload_dir' => dirname($this->get_server_var('SCRIPT_FILENAME')).'/files/',
-            'upload_url' => '/uploads/',
+            'upload_dir' => 'files/',
+            'upload_url' => $this->get_full_url().'/files/',
             'input_stream' => 'php://input',
             'user_dirs' => false,
             'mkdir_mode' => 0755,
