0a15bc8f941313e55acb6ed16f9fb5bff7a1dfb0
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-08 17:38:23 -0400

    commit

--- storage-iso-wizard.php
+++ storage-iso-wizard.php
@@ -61,11 +61,15 @@ require('navbar.php');
 
 
                       <div class="container">
-                          <h4>ISO File Upload</h4>
+                          <h1>jQuery File Upload Demo</h1>
+                          <h2 class="lead">Basic Plus version</h2>
+
+
                           <br>
                           <!-- The fileinput-button span is used to style the file input field as button -->
                           <span class="btn btn-success fileinput-button">
-                              <span>Select ISO file...</span>
+                              <i class="glyphicon glyphicon-plus"></i>
+                              <span>Add files...</span>
                               <!-- The file input field used as target for the file upload widget -->
                               <input id="fileupload" type="file" name="files[]" multiple>
                           </span>
@@ -78,7 +82,20 @@ require('navbar.php');
                           <!-- The container for the uploaded files -->
                           <div id="files" class="files"></div>
                           <br>
-
+                          <div class="panel panel-default">
+                              <div class="panel-heading">
+                                  <h3 class="panel-title">Demo Notes</h3>
+                              </div>
+                              <div class="panel-body">
+                                  <ul>
+                                      <li>The maximum file size for uploads in this demo is <strong>999 KB</strong> (default file size is unlimited).</li>
+                                      <li>Only image files (<strong>JPG, GIF, PNG</strong>) are allowed in this demo (by default there is no file type restriction).</li>
+                                      <li>Uploaded files will be deleted automatically after <strong>5 minutes or less</strong> (demo files are stored in memory).</li>
+                                      <li>You can <strong>drag &amp; drop</strong> files from your desktop on this webpage (see <a href="https://github.com/blueimp/jQuery-File-Upload/wiki/Browser-support">Browser support</a>).</li>
+                                      <li>Please refer to the <a href="https://github.com/blueimp/jQuery-File-Upload">project website</a> and <a href="https://github.com/blueimp/jQuery-File-Upload/wiki">documentation</a> for more information.</li>
+                                      <li>Built with the <a href="http://getbootstrap.com/">Bootstrap</a> CSS framework and Icons from <a href="http://glyphicons.com/">Glyphicons</a>.</li>
+                                  </ul>
+                              </div>
                           </div>
                       </div>
 
