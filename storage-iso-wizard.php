4783fca850ed586ef557bceac1cdd7a68f402a64
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-09 05:33:05 -0400

    commit

--- storage-iso-wizard.php
+++ storage-iso-wizard.php
@@ -1,5 +1,6 @@
 <?php
 require('header.php');
+
 ?>
 
 <?php
@@ -115,8 +116,9 @@ function newExtenstion(f) {
                   <div class="col-sm-10">
                     <div class="form-group">
                       <div class="custom-file">
+                        
 
-                  <input id="fileupload" type="file" name="files[]" data-url="server/php/" multiple>
+                  <input id="fileupload" type="file" name="files[]" data-url="server/php/" class="btn btn-success fileinput-button" multiple>
                   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
                   <script src="jQuery-File-Upload/js/vendor/jquery.ui.widget.js"></script>
                   <script src="jQuery-File-Upload/js/jquery.iframe-transport.js"></script>
