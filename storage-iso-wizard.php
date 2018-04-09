ea7bb455f2d5454f074910d8cce3c5e62032ab7c
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-09 06:02:46 -0400

    commit

--- storage-iso-wizard.php
+++ storage-iso-wizard.php
@@ -129,7 +129,7 @@ function newExtenstion(f) {
                             <!-- The fileinput-button span is used to style the file input field as button -->
                             <span class="btn btn-success fileinput-button">
                                 <i class="glyphicon glyphicon-plus"></i>
-                                <span>Add files...</span>
+                                <span>Add ISO...</span>
                                 <!-- The file input field used as target for the file upload widget -->
                                 <input id="fileupload" type="file" name="files[]" multiple>
                             </span>
