ee4b714f26743a840a81856caaf53e2b46b96775
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-09 21:19:27 -0400

    commit

--- upload.php
+++ upload.php
@@ -8,10 +8,7 @@ include('navbar.php');
 
 <div class="panel-header panel-header-sm"></div>
 
-<div class="content">
-  <div class="row">
-    <div class="col-md-12">
-      <div class="card">
+
 
 <div class="container">
     <h4>ISO Image Upload</h4>
@@ -37,10 +34,7 @@ include('navbar.php');
     </div>
 </div>
 
-</div>
-</div>
-</div>
-</div>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
 <script src="jQuery-File-Upload/js/vendor/jquery.ui.widget.js"></script>
