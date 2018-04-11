3dba3d11f9306727ffed21c683dae47777891450
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-11 17:30:45 -0400

    commit

---
+++ pages/domain-list.php
@@ -0,0 +1,159 @@
+<?php
+require('header.php');
+require('navbar.php');
+
+$uuid = $_GET['uuid']; //grab the $uuid variable from $_GET, only used for actions below
+$action = $_GET['action']; //grab the $action variable from $_GET
+$domName = $lv->domain_get_name_by_uuid($uuid); //get the name of virtual machine with $uuid is present
+$dom = $lv->get_domain_object($domName); //gets the resource id for a domain
+
+//This will turn a shutdown virtual machine on. This option in only given when a machine is shutdown
+if ($action == 'domain-start') {
+  $msg = $lv->domain_start($domName) ? "Domain has been started successfully" : 'Error while starting domain: '.$lv->get_last_error();
+}
+
+//This will pause a virtual machine and temporaily save it's state
+if ($action == 'domain-pause') {
+  $msg = $lv->domain_suspend($domName) ? "Domain has been paused successfully" : 'Error while pausing domain: '.$lv->get_last_error();
+}
+
+//This will resume a paused virtual machine. Option is given only if a machine is paused
+if ($action == 'domain-resume') {
+  $msg = $lv->domain_resume($domName) ? "Domain has been resumed successfully" : 'Error while resuming domain: '.$lv->get_last_error();
+}
+
+//This is used to gracefully shutdown the guest.
+//There are many reasons why a guest cannot gracefully shutdown so if it can't, let the user know that
+if ($action == 'domain-stop') {
+  $msg = $lv->domain_shutdown($domName) ? "Domain has been stopped successfully" : 'Error while stopping domain: '.$lv->get_last_error();
+  $actioninfo = $lv->domain_get_info($dom);
+  $actionstate = $lv->domain_state_translate($actioninfo['state']);
+  if ($actionstate == "running"){
+    $msg = "Domain is unable to shutdown gracefully. It will need to be forcefully turned off";
+  }
+}
+
+//This will forcefully shutdown the virtual machine guest
+if ($action == 'domain-destroy') {
+  $msg = $lv->domain_destroy($domName) ? "Domain has been destroyed successfully" : 'Error while destroying domain: '.$lv->get_last_error();
+}
+ ?>
+
+<!-- page content -->
+<div class="right_col" role="main">
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Virtual Guests: <small>domain list</small></h3>
+      </div>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2>Guests</h2>
+            <ul class="nav navbar-right panel_toolbox">
+              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Settings 1</a>
+                  </li>
+                  <li><a href="#">Settings 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li><a class="close-link"><i class="fa fa-close"></i></a>
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+
+            <p><a href="guest-wizard.php"><i class="fas fa-plus"></i> Create new guest </a></p>
+
+            <!-- start project list -->
+            <table class="table table-striped projects">
+              <thead>
+                <tr>
+                  <th style="width: 1%">#</th>
+                  <th style="width: 20%">Guest Name</th>
+                  <th>Virtual CPUs</th>
+                  <th>Memory</th>
+                  <th>Disks</th>
+                  <th>State</th>
+                  <th style="width: 20%">#Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+
+                <tbody>
+                  <?php
+                  $doms = $lv->get_domains();
+                  foreach ($doms as $name) {
+                    $dom = $lv->get_domain_object($name);
+                    $uuid = libvirt_domain_get_uuid_string($dom);
+                    $active = $lv->domain_is_active($dom);
+                    $info = $lv->domain_get_info($dom);
+                    $mem = number_format($info['memory'] / 1024, 0, '.', '').' MB';
+                    $cpu = $info['nrVirtCpu'];
+                    $state = $lv->domain_state_translate($info['state']);
+                    $id = $lv->domain_get_id($dom);
+                    $arch = $lv->domain_get_arch($dom);
+                    $vnc = $lv->domain_get_vnc_port($dom);
+                    $nics = $lv->get_network_cards($dom);
+
+  	                if (($diskcnt = $lv->get_disk_count($dom)) > 0) {
+                      $disks = $diskcnt.' / '.$lv->get_disk_capacity($dom);
+                      $diskdesc = 'Current physical size: '.$lv->get_disk_capacity($dom, true);
+                    } else {
+                      $disks = '-';
+                      $diskdesc = '';
+                    }
+
+                    unset($tmp);
+                    unset($dom);
+
+  	                echo "<tr>" .
+                      "<td>#</td>" .
+                      "<td>" .
+                        "<a href=\"guest-single.php?uuid=$uuid\">$name</a>" .
+                        "<small>State:$state</small>" .
+                      "</td>" .
+                      "<td>$cpu</td>" .
+                      "<td>$mem</td>" .
+                      "<td title='$diskdesc'>$disks</td>" .
+                      "<td>$state</td>" .
+                      "<td>";
+
+  	                if ($lv->domain_is_running($name)){
+                     echo "<a href=\"?action=domain-stop&amp;uuid=$uuid\">Shutdown</a> | <a href=\"?action=domain-destroy&amp;uuid=$uuid\">Turn off</a> | <a href=\"?action=domain-pause&amp;uuid=$uuid\">Pause</a>";
+                    } else if ($lv->domain_is_paused($name)){
+                     echo "<a href=\"?action=domain-resume&amp;uuid=$uuid\">Resume</a>";
+                    } else {
+                     echo "<a href=\"?action=domain-start&amp;uuid=$uuid\" class=\"btn btn-primary btn-xs\"><i class=\"fas fa-power-off\"></i> Power on</a>";
+                    }
+                    echo "</td>";
+  	                echo "</tr>";
+                  }
+                  ?>
+                </tr>
+              </tbody>
+            </table>
+            <!-- end project list -->
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- /page content -->
+
+<?php
+require('footer.php');
+?>
---
+++ pages/footer.php
@@ -0,0 +1,54 @@
+<!-- footer content -->
+<footer>
+  <div class="pull-right">
+    Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+  </div>
+  <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
+</div>
+</div>
+
+<!-- jQuery -->
+<script src="../vendors/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="../vendors/fastclick/lib/fastclick.js"></script>
+<!-- NProgress -->
+<script src="../vendors/nprogress/nprogress.js"></script>
+<!-- Chart.js -->
+<script src="../vendors/Chart.js/dist/Chart.min.js"></script>
+<!-- gauge.js -->
+<script src="../vendors/gauge.js/dist/gauge.min.js"></script>
+<!-- bootstrap-progressbar -->
+<script src="../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+<!-- iCheck -->
+<script src="../vendors/iCheck/icheck.min.js"></script>
+<!-- Skycons -->
+<script src="../vendors/skycons/skycons.js"></script>
+<!-- Flot -->
+<script src="../vendors/Flot/jquery.flot.js"></script>
+<script src="../vendors/Flot/jquery.flot.pie.js"></script>
+<script src="../vendors/Flot/jquery.flot.time.js"></script>
+<script src="../vendors/Flot/jquery.flot.stack.js"></script>
+<script src="../vendors/Flot/jquery.flot.resize.js"></script>
+<!-- Flot plugins -->
+<script src="../vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+<script src="../vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+<script src="../vendors/flot.curvedlines/curvedLines.js"></script>
+<!-- DateJS -->
+<script src="../vendors/DateJS/build/date.js"></script>
+<!-- JQVMap -->
+<script src="../vendors/jqvmap/dist/jquery.vmap.js"></script>
+<script src="../vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<script src="../vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+<!-- bootstrap-daterangepicker -->
+<script src="../vendors/moment/min/moment.min.js"></script>
+<script src="../vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+
+<!-- Custom Theme Scripts -->
+<script src="../build/js/custom.min.js"></script>
+
+</body>
+</html>
