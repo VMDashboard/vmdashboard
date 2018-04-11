9aa50751057b113e90f2a4320ed8cbdc4a7b4e60
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-11 17:47:49 -0400

    commit

--- pages/header.php
+++ pages/header.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="images/favicon.ico" type="image/ico" />
+	<link rel="icon" href="../production/images/favicon.ico" type="image/ico" />
 
     <title>openVM</title>
 
