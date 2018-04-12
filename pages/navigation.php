69ccd56b31af7fe8964af37b91affadd924e5a6c
Matthew <matthew@mattbook.hsd1.pa.comcast.net>
2018-04-11 21:57:14 -0400

    commit

--- pages/navigation.php
+++ pages/navigation.php
@@ -26,7 +26,7 @@
           <!-- sidebar menu -->
           <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
             <div class="menu_section">
-              <h3>General</h3>
+              <h3>Navigation</h3>
               <ul class="nav side-menu">
                 <li><a><i class="fa fa-home"></i> Host <span class="fa fa-chevron-down"></span></a>
                   <ul class="nav child_menu">
@@ -41,40 +41,21 @@
                     <li><a href="domain-wizard.php">Create new domain</a></li>
                   </ul>
                 </li>
-                <li><a><i class="fa fa-desktop"></i> UI Elements <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-desktop"></i> Storage <span class="fa fa-chevron-down"></span></a>
                   <ul class="nav child_menu">
-                    <li><a href="general_elements.html">General Elements</a></li>
-                    <li><a href="media_gallery.html">Media Gallery</a></li>
-                    <li><a href="typography.html">Typography</a></li>
-                    <li><a href="icons.html">Icons</a></li>
-                    <li><a href="glyphicons.html">Glyphicons</a></li>
-                    <li><a href="widgets.html">Widgets</a></li>
-                    <li><a href="invoice.html">Invoice</a></li>
-                    <li><a href="inbox.html">Inbox</a></li>
-                    <li><a href="calendar.html">Calendar</a></li>
+                    <li><a href="general_elements.html">Storage Pools</a></li>
+                    <li><a href="media_gallery.html">Create new storage pool</a></li>
+                    <li><a href="media_gallery.html">Create new storage volume</a></li>
+                    <li><a href="media_gallery.html">Upload new ISO image</a></li>
                   </ul>
                 </li>
-                <li><a><i class="fa fa-table"></i> Tables <span class="fa fa-chevron-down"></span></a>
+                <li><a><i class="fa fa-table"></i> Networking <span class="fa fa-chevron-down"></span></a>
                   <ul class="nav child_menu">
-                    <li><a href="tables.html">Tables</a></li>
-                    <li><a href="tables_dynamic.html">Table Dynamic</a></li>
+                    <li><a href="tables.html">Private Networks</a></li>
+                    <li><a href="tables_dynamic.html">Create new network</a></li>
                   </ul>
                 </li>
-                <li><a><i class="fa fa-bar-chart-o"></i> Data Presentation <span class="fa fa-chevron-down"></span></a>
-                  <ul class="nav child_menu">
-                    <li><a href="chartjs.html">Chart JS</a></li>
-                    <li><a href="chartjs2.html">Chart JS2</a></li>
-                    <li><a href="morisjs.html">Moris JS</a></li>
-                    <li><a href="echarts.html">ECharts</a></li>
-                    <li><a href="other_charts.html">Other Charts</a></li>
-                  </ul>
-                </li>
-                <li><a><i class="fa fa-clone"></i>Layouts <span class="fa fa-chevron-down"></span></a>
-                  <ul class="nav child_menu">
-                    <li><a href="fixed_sidebar.html">Fixed Sidebar</a></li>
-                    <li><a href="fixed_footer.html">Fixed Footer</a></li>
-                  </ul>
-                </li>
+                
               </ul>
             </div>
             <div class="menu_section">
