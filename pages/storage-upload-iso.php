5acd6158b4ca863e652e17296f341b1bfa3cb89a
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-13 11:19:55 -0400

    commit

--- pages/storage-upload-iso.php
+++ pages/storage-upload-iso.php
@@ -79,7 +79,7 @@ require('navigation.php');
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
-<script src="jQuery-File-Upload/js/vendor/jquery.ui.widget.js"></script>
+<script src="../apps/jQuery-File-Upload/js/vendor/jquery.ui.widget.js"></script>
 <!-- The Load Image plugin is included for the preview images and image resizing functionality -->
 <script src="https://blueimp.github.io/JavaScript-Load-Image/js/load-image.all.min.js"></script>
 <!-- The Canvas to Blob plugin is included for image resizing functionality -->
@@ -87,19 +87,19 @@ require('navigation.php');
 <!-- Bootstrap JS is not required, but included for the responsive demo navigation -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
-<script src="jQuery-File-Upload/js/jquery.iframe-transport.js"></script>
+<script src="../apps/jQuery-File-Upload/js/jquery.iframe-transport.js"></script>
 <!-- The basic File Upload plugin -->
-<script src="jQuery-File-Upload/js/jquery.fileupload.js"></script>
+<script src="../apps/jQuery-File-Upload/js/jquery.fileupload.js"></script>
 <!-- The File Upload processing plugin -->
-<script src="jQuery-File-Upload/js/jquery.fileupload-process.js"></script>
+<script src="../apps/jQuery-File-Upload/js/jquery.fileupload-process.js"></script>
 <!-- The File Upload image preview & resize plugin -->
-<script src="jQuery-File-Upload/js/jquery.fileupload-image.js"></script>
+<script src="../apps/jQuery-File-Upload/js/jquery.fileupload-image.js"></script>
 <!-- The File Upload audio preview plugin -->
-<script src="jQuery-File-Upload/js/jquery.fileupload-audio.js"></script>
+<script src="../apps/jQuery-File-Upload/js/jquery.fileupload-audio.js"></script>
 <!-- The File Upload video preview plugin -->
-<script src="jQuery-File-Upload/js/jquery.fileupload-video.js"></script>
+<script src="../apps/jQuery-File-Upload/js/jquery.fileupload-video.js"></script>
 <!-- The File Upload validation plugin -->
-<script src="jQuery-File-Upload/js/jquery.fileupload-validate.js"></script>
+<script src="../apps/jQuery-File-Upload/js/jquery.fileupload-validate.js"></script>
 <script>
 /*jslint unparam: true, regexp: true */
 /*global window, $ */
