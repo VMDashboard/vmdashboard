5150c49d79ef4b2eb8ba3dc6f41af622ad3f3579
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-13 11:27:05 -0400

    commit

--- pages/storage-upload-iso.php
+++ pages/storage-upload-iso.php
@@ -44,11 +44,16 @@ require('navigation.php');
 
 
 
-          
-            <!-- The global progress bar -->
-            <div id="progress" class="progress">
-              <div class="progress-bar progress-bar-success"></div>
-            </div>
+            <!-- The fileinput-button span is used to style the file input field as button -->
+            <span class="btn btn-plain fileinput-button">
+              <i class="glyphicon glyphicon-plus"></i>
+              <span>Add files...</span>
+              <!-- The file input field used as target for the file upload widget -->
+              <input id="fileupload" type="file" name="files[]">
+            </span>
+            <br>
+            <br>
+            
             <!-- The container for the uploaded files -->
             <div id="files" class="files"></div>
             <br>
