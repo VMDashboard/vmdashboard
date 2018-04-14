1fea425666c185966d8f5b07ffc8d6d94f2cf130
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-14 09:22:34 -0400

    commit

--- pages/storage-upload-iso.php
+++ pages/storage-upload-iso.php
@@ -215,16 +215,6 @@ $(function () {
 
 
 
-<!-- FastClick -->
-<script src="../vendors/fastclick/lib/fastclick.js"></script>
-<!-- NProgress -->
-<script src="../vendors/nprogress/nprogress.js"></script>
-<!-- Chart.js -->
-<script src="../vendors/Chart.js/dist/Chart.min.js"></script>
-<!-- gauge.js -->
-<script src="../vendors/gauge.js/dist/gauge.min.js"></script>
-<!-- bootstrap-progressbar -->
-<script src="../vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
 <!-- iCheck -->
 <script src="../vendors/iCheck/icheck.min.js"></script>
 <!-- Skycons -->
