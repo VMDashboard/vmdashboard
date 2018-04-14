c668f73e5e97be4c47f0a153abe6a3ffc0c462fa
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-14 13:10:58 -0400

    commit

--- pages/storage-upload-iso.php
+++ pages/storage-upload-iso.php
@@ -104,6 +104,8 @@ echo "<div class=\"col-md-1 col-sm-2 col-xs-4\" style=\"text-align:center;\">
   </div>
 </div>
 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
 <script src="../apps/jQuery-File-Upload/js/vendor/jquery.ui.widget.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
