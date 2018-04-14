67057d725ec029dbd4ad16036f798cb38381c5a8
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-14 13:11:50 -0400

    commit

--- pages/storage-upload-iso.php
+++ pages/storage-upload-iso.php
@@ -215,8 +215,6 @@ $(function () {
 });
 </script>
 
-<!-- jQuery -->
-<script src="../vendors/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap -->
 <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- FastClick -->
