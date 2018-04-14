a0adb11e62ab2599b8a5d341c666cbc2193fbeff
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-14 09:02:36 -0400

    commit

--- pages/storage-upload-iso.php
+++ pages/storage-upload-iso.php
@@ -68,7 +68,7 @@ if ($files){
 }
 for ($i = 0; $i < sizeof($files); $i++) {
   $iso_name = basename($files[$i]); //strips off the relative filepath and returns just filename
-echo "<div style=\"text-align:center; width:10%; display: block;\">
+echo "<div class=\"col-md-1 col-sm-1 col-xs-1\" style=\"text-align:center;\">
       <center>
         <img style=\"width: 75%; display: block;\" src=\"../assets/img/cddvd.png\" alt=\"image\" />
       </center>
