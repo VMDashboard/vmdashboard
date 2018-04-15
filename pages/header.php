ae6cefbf707a1217d9d0f8fc28c8c5645bbe2f9e
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-15 19:54:00 -0400

    commit

--- pages/header.php
+++ pages/header.php
@@ -19,7 +19,15 @@
     <!-- iCheck -->
     <link href="../vendors/iCheck/skins/flat/green.css" rel="stylesheet">
 
-    
+    <!-- bootstrap-progressbar -->
+    <link href="../vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="../vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet"/>
+    <!-- bootstrap-daterangepicker -->
+    <link href="../vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="../build/css/custom.min.css" rel="stylesheet">
 
     <!-- ISO Uploads -->
     <!-- CSS to style the file input field as button and adjust the Bootstrap progress bars -->
