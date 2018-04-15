3b21c503e914a46eec2f7c9c493f3c5eebb382a0
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-15 09:19:11 -0400

    commit

--- pages/navigation.php
+++ pages/navigation.php
@@ -9,19 +9,8 @@
 
           <div class="clearfix"></div>
 
-          <!-- menu profile quick info -->
-          <div class="profile clearfix">
-            <div class="profile_pic">
-              <img src="../production/images/img.jpg" alt="..." class="img-circle profile_img">
-            </div>
-            <div class="profile_info">
-              <span>Welcome,</span>
-              <h2>openVM</h2>
-            </div>
-          </div>
-          <!-- /menu profile quick info -->
 
-          <br />
+          <br />   <br />   <br /> 
 
           <!-- sidebar menu -->
           <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
