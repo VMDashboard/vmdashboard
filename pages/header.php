828a70eb23ff85576c09ba24498a70dc2e97bc73
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-17 21:52:38 -0400

    commit

--- pages/header.php
+++ pages/header.php
@@ -35,11 +35,7 @@
 
   </head>
 
-  <?php
-  //shell_exec("./noVNC/utils/websockify/run --web /var/www/html/openVM/noVNC/ --target-config ./tokens.list 192.168.2.10:6080 >/dev/null 2>&1 &");
-  $fileDir = dirname(__FILE__);
-  shell_exec("./../apps/noVNC/utils/websockify/run --web $fileDir/../apps/noVNC/ --target-config ../tokens.list 6080 > ../novnc_err.log 2>&1 &");
-  ?>
+
 
   <?php
   //bring in the libvirt class and methods
