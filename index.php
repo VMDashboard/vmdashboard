6a8c2d28c77271ffd3d11c15c1465ebb6791ecac
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-18 06:23:15 -0400

    commit

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 //shell_exec("./noVNC/utils/websockify/run --web /var/www/html/openVM/noVNC/ --target-config ./tokens.list 192.168.2.10:6080 >/dev/null 2>&1 &");
 $fileDir = dirname(__FILE__);
-shell_exec("./apps/noVNC/utils/websockify/run --web $fileDir/apps/noVNC/ --target-config ./tokens.list 6080 > novnc_err.log 2>&1 &");
+shell_exec("./apps/noVNC/utils/websockify/run --web $fileDir/apps/noVNC/ --target-config ./tokens.list 6080 > logs/novnc.log 2>&1 &");
 
 
 
