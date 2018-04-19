8d6ff7b970fe7bf59670da8c9264d7191ff391a7
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-19 09:02:12 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -2,6 +2,45 @@
 require('header.php');
 require('navigation.php'); ?>
 
+
+<!-- page content -->
+<div class="right_col" role="main">
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Domains</h3>
+      </div>
+    </div>
+
+    <div class="clearfix"></div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="x_panel">
+          <div class="x_title">
+            <h2>Domain List</h2>
+            <ul class="nav navbar-right panel_toolbox">
+              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">Settings 1</a>
+                  </li>
+                  <li><a href="#">Settings 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li><a class="close-link"><i class="fa fa-close"></i></a>
+              </li>
+            </ul>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+
+
+
+
 <?php
 $xmlstr = "
 <domain type='kvm'>
@@ -109,7 +148,13 @@ echo "Domain Name: " . $domain_name;
 
 
 
-
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<!-- /page content -->
 
 
 
