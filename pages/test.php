f618f9cb53e71c1d5d2f941245efa1d783924d63
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-20 06:18:03 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -135,7 +135,7 @@ $second = $xml->os->boot[1][dev];
 echo "Second boot device: " . $second . "<br>";
 
 
-$interface = $xml->devices[0]->addChild('interface[type]','direct');
+$interface = $xml->devices->addChild('interface','direct');
 $interface->addChild('mac[address]', '52:54:00:66:55:44');
 
 echo "<textarea>";
