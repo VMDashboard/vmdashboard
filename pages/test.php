b3c6d259475bdae504bfd412218a7336532a5442
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-21 07:11:34 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -137,6 +137,9 @@ echo "Memory Unit: " . $unit . "<br>";
 $second = $xml->os->boot[1][dev];
 echo "Second boot device: " . $second . "<br>";
 
+$boot = $xml->os->boot;
+var_dump($boot);
+
 //add a new interface
 $interface = $xml->devices->addChild('interface');
 $interface->addAttribute('type','direct');
