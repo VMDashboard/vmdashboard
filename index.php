15273d8d7545bc5c94962d6ba06c3a5e8a6a5b46
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-24 14:59:47 -0400

    commit

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+//create an initial setup page. Have it configure database, username, password,
+//default storage pool and default network (may already be created by default).
+//initial setup page will create config.php after complete
+
+
 
 //shell_exec("./noVNC/utils/websockify/run --web /var/www/html/openVM/noVNC/ --target-config ./tokens.list 192.168.2.10:6080 >/dev/null 2>&1 &");
 $fileDir = dirname(__FILE__);
