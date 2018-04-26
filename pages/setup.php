44d89d9bf6bcae1c7a18a297b552994555548f73
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-25 22:38:07 -0400

    commit

--- pages/setup.php
+++ pages/setup.php
@@ -28,7 +28,7 @@
     <div>
       <a class="hiddenanchor" id="signup"></a>
       <a class="hiddenanchor" id="signin"></a>
-
+<form>
       <div class="login_wrapper">
         <div class="animate form login_form">
           <section class="login_content">
@@ -68,13 +68,13 @@
                   <p>©2018 All Rights Reserved.</p>
                 </div>
               </div>
-            
+            </form>
           </section>
         </div>
 
         <div id="register" class="animate form registration_form">
           <section class="login_content">
-
+            <form>
               <h1>Create Account</h1>
               <div>
                 <input type="text" class="form-control" placeholder="Username" required="" />
@@ -108,6 +108,7 @@
           </section>
         </div>
       </div>
+    </form>
     </div>
   </body>
 </html>
