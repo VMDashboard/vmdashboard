1e98ba70f7fc38170e95855d8786634718d8ed1a
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-26 14:00:59 -0400

    commit

--- pages/setup.php
+++ pages/setup.php
@@ -48,9 +48,9 @@ if (isset($_POST['account'])){
     // Creating the SQL statement
 
     //Creating the users tables
-    $sql = "CREATE TABLE users (username varchar(255), email varchar(255), password varchar(255));";
-    $conn->query($sql);
-    
+    //$sql = "CREATE TABLE users (username varchar(255), email varchar(255), password varchar(255));";
+    //$conn->query($sql);
+
     $sql = "INSERT INTO users (username, email, password)
       VALUES ('$username', '$email', '$hash');";
     // Executing the SQL statement
