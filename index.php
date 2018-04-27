6c3d53c07d1f0777040c1f95ae0f825ea2fa4463
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-26 22:36:16 -0400

    commit

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
+
 //create an initial setup page. Have it configure database, username, password,
 //default storage pool and default network (may already be created by default).
 //initial setup page will create config.php after complete
-
 
 
 //shell_exec("./noVNC/utils/websockify/run --web /var/www/html/openVM/noVNC/ --target-config ./tokens.list 192.168.2.10:6080 >/dev/null 2>&1 &");
@@ -11,16 +11,14 @@ $fileDir = dirname(__FILE__);
 shell_exec("./apps/noVNC/utils/websockify/run --web $fileDir/apps/noVNC/ --target-config ./tokens.list 6080 > logs/novnc.log 2>&1 &");
 
 if (isset($_SESSION['username'])) {
-  echo "YES";
   header('Location: pages/domain-list.php');
 }
 
 $path = dirname(__FILE__) . "/config.php";
-echo $path;
 if (file_exists($path)) {
-  //header('Location: pages/login.php');
+  header('Location: pages/login.php');
 } else {
-//header('Location: pages/setup.php');
+header('Location: pages/setup.php');
 }
 
  ?>
