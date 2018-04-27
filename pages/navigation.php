4b3d895f831a4e591a77b8de8a3fb3f2ce6e2fc7
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-26 22:39:07 -0400

    commit

--- pages/navigation.php
+++ pages/navigation.php
@@ -58,7 +58,7 @@
             <a data-toggle="tooltip" data-placement="top" title="Lock">
               <span class="glyphicon glyphicon-eye-close" aria-hidden="true"></span>
             </a>
-            <a data-toggle="tooltip" data-placement="top" title="Logout" href="login.html">
+            <a data-toggle="tooltip" data-placement="top" title="Logout" href="../index.php?action=logout">
               <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
             </a>
           </div>
