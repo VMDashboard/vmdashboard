12d9507fbf4fde26a526e43f077ae0a332bb7c59
matthewalanpenning <matthewalanpenning@gmail.com>
2018-04-30 11:54:05 -0400

    commit

--- index.php
+++ index.php
@@ -11,12 +11,14 @@ $path = dirname(__FILE__) . "/config.php";
 $fileDir = dirname(__FILE__);
 shell_exec("./apps/noVNC/utils/websockify/run --web $fileDir/apps/noVNC/ --target-config ./tokens.list 6080 > logs/novnc.log 2>&1 &");
 
-
+//currently using GET to at the index page to indicate logout, may switch to SESSION VARIABLE
 $action = $_GET['action'];
 if ($action == "logout") {
   unset($_SESSION['username']);
 }
 
+
+//Redirect based on login session or initial setup complete
 if (isset($_SESSION['username'])) {
   header('Location: pages/domain-list.php');
 } elseif (file_exists($path)) {
