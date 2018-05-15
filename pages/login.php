184182d7b08d0e432b34b0f1fdd140b3b0f622c9
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-15 14:34:44 -0400

    commit

--- pages/login.php
+++ pages/login.php
@@ -38,7 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" href="../assets/img/logo3.png" type="image/png" />
+    <link rel="icon" href="../assets/img/squarelogo.png" type="image/png" />
 
     <title>openVM</title>
 
--- pages/setup-database.php
+++ pages/setup-database.php
@@ -49,7 +49,7 @@ if (isset($_POST['database'])){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="icon" href="../assets/img/logo3.png" type="image/png" />
+    <link rel="icon" href="../assets/img/squarelogo.png" type="image/png" />
 
     <title>openVM</title>
 
