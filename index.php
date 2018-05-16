eb0daa60d664bc1b45665778c9ba1ff388380f3b
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-16 19:57:03 -0400

    commit

--- index.php
+++ index.php
@@ -9,8 +9,13 @@ $path = dirname(__FILE__) . "/config.php";
 
 //shell_exec("./noVNC/utils/websockify/run --web /var/www/html/openVM/noVNC/ --target-config ./tokens.list 192.168.2.10:6080 >/dev/null 2>&1 &");
 $fileDir = dirname(__FILE__);
-shell_exec("./apps/noVNC/utils/websockify/run --web $fileDir/apps/noVNC/ --target-config ./tokens.list 6080 > logs/novnc.log 2>&1 &");
+$protocol = isset($_SERVER['HTTPS']) ? "https://" : "http://";
 
+if ($protocol == "http://"){
+  shell_exec("./apps/noVNC/utils/websockify/run --web $fileDir/apps/noVNC/ --cert ./apps/noVNC/utils/self.pem --target-config ./tokens.list 6080 > logs/novnc.log 2>&1 &");
+} else{
+  shell_exec("./apps/noVNC/utils/websockify/run --web $fileDir/apps/noVNC/ --target-config ./tokens.list 6080 > logs/novnc.log 2>&1 &");
+}
 //currently using GET to at the index page to indicate logout, may switch to SESSION VARIABLE
 $action = $_GET['action'];
 if ($action == "logout") {
---
+++ pages/test.php
@@ -0,0 +1,13 @@
+<?php
+
+require('header.php');
+require('navigation.php');
+
+
+$dir = X509_get_default_cert_file();
+
+var_dump($dir);
+
+require('footer.php');
+
+ ?>
