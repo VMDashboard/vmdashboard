604e67069ff8cce22cd39d3c4eef57316f3a4781
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-17 12:58:55 -0400

    commit

--- pages/host-pci.php
+++ pages/host-pci.php
@@ -1,10 +1,14 @@
 <?php
+// If the SESSION has not started, start it now
 if (!isset($_SESSION)) {
     session_start();
 }
+// If there is no username, then we need to send them to the login
 if (!isset($_SESSION['username'])){
-  header('Location: ../index.php');
+  header('Location: login.php');
 }
+// We are now going to grab any POST data and put in in SESSION data, then clear it.
+// This will prevent and reloading the webpage to resubmit and action.
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $_SESSION['action'] = $_POST['action'];
     $_SESSION['name'] = $_POST['name'];
@@ -12,6 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     header("Location: ".$_SERVER['PHP_SELF']);
     exit;
 }
+// Time to bring in the header, sidebar, and navigation menus
 require('header.php');
 require('navigation.php');
 ?>
@@ -41,24 +46,32 @@ require('navigation.php');
 
           <div class="col-md-9 col-sm-9 col-xs-12">
             <?php
+            // Time to get all information on the host
             $tmp = $lv->host_get_node_info();
+            // Let's start the $ret without any data, it will be used to display returned XML info
             $ret = false;
 
+            // Check for SESSION variables to see if we need to retieve XML data
             if (array_key_exists('action', $_SESSION)) {
               $name = $_SESSION['name'];
               if ($_SESSION['action'] == 'dumpxml')
                 $ret = 'XML dump of node device <i>'.$name.'</i>:<br/><br/>'.htmlentities($lv->get_node_device_xml($name, false));
+                //Unset the SESSION variables
                 unset($_SESSION['name']);
                 unset($_SESSION['action']);
             }
 
+            //If we have returned XML data, display it
             if ($ret){
               echo "<pre>$ret</pre>";
               echo "<br /><br />";
             }
 
+            //Time to retrieve the information about the host and place it in a table
             $tmp = $lv->get_node_device_cap_options();
             for ($i = 0; $i < sizeof($tmp); $i++) {
+
+              //Just pull out PCI data
               if ($tmp[$i] == "pci"){
                 echo "<h4>{$tmp[$i]}</h4>";
                 $tmp1 = $lv->get_node_devices($tmp[$i]);
@@ -75,7 +88,7 @@ require('navigation.php');
 
                 for ($ii = 0; $ii < sizeof($tmp1); $ii++) {
                   $tmp2 = $lv->get_node_device_information($tmp1[$ii]);
-                  //$act = "<a href=\"?action={$_GET['action']}&amp;action=dumpxml&amp;name={$tmp2['name']}\">Dump configuration</a>";
+                  //Actions will be a form button that will submit info using POST
                   $act = "<form method=\"post\" action=\"\">
                     <input type=\"hidden\" name=\"action\" value=\"dumpxml\">
                     <input type=\"hidden\" name=\"name\" value=\"{$tmp2['name']}\">
