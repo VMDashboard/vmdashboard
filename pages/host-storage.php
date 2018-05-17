0a2d751f4914cd401b5b62fc5f76283971e265a9
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-17 06:00:41 -0400

    commit

--- pages/host-storage.php
+++ pages/host-storage.php
@@ -7,43 +7,28 @@ require('header.php');
 require('navigation.php');
 ?>
 
-        <!-- page content -->
-        <div class="right_col" role="main">
-          <div class="">
-            <div class="page-title">
-              <div class="title_left">
-                <h3>Host</h3>
-              </div>
+<!-- page content -->
+<div class="right_col" role="main">
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Host</h3>
+      </div>
+    </div>
+  </div>
 
-              </div>
-            </div>
+  <div class="clearfix"></div>
 
-            <div class="clearfix"></div>
+  <div class="row">
+    <div class="col-md-12">
+      <div class="x_panel">
+        <div class="x_title">
+          <h2>Host Storage: <?php echo $hn; ?></h2>
 
-            <div class="row">
-              <div class="col-md-12">
-                <div class="x_panel">
-                  <div class="x_title">
-                    <h2>Host Storage: <?php echo $hn; ?></h2>
-                    <ul class="nav navbar-right panel_toolbox">
-                      <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-                      </li>
-                      <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                        <ul class="dropdown-menu" role="menu">
-                          <li><a href="#">Settings 1</a>
-                          </li>
-                          <li><a href="#">Settings 2</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li><a class="close-link"><i class="fa fa-close"></i></a>
-                      </li>
-                    </ul>
-                    <div class="clearfix"></div>
-                  </div>
+          <div class="clearfix"></div>
+        </div>
 
-                  <div class="x_content">
+        <div class="x_content">
 
                     <div class="col-md-9 col-sm-9 col-xs-12">
                       <?php
