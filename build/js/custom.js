42701a53b068a42a8c7f4a7c99f90b3292c6a7fa
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-17 20:29:08 -0400

    commit

--- build/js/custom.js
+++ build/js/custom.js
@@ -99,6 +99,36 @@ var setContentHeight = function () {
         }
     });
 
+    $SIDEBAR_MENU.find('a').on('mouseleave', function(ev) {
+  	  console.log('clicked - sidebar_menu');
+          var $li = $(this).parent();
+
+          if ($li.is('.active')) {
+              $li.removeClass('active active-sm');
+              $('ul:first', $li).slideUp(function() {
+                  setContentHeight();
+              });
+          } else {
+              // prevent closing menu if we are on child menu
+              if (!$li.parent().is('.child_menu')) {
+                  $SIDEBAR_MENU.find('li').removeClass('active active-sm');
+                  $SIDEBAR_MENU.find('li ul').slideUp();
+              }else
+              {
+  				if ( $BODY.is( ".nav-sm" ) )
+  				{
+  					$SIDEBAR_MENU.find( "li" ).removeClass( "active active-sm" );
+  					$SIDEBAR_MENU.find( "li ul" ).slideUp();
+  				}
+  			}
+              $li.addClass('active');
+
+              $('ul:first', $li).slideDown(function() {
+                  setContentHeight();
+              });
+          }
+      });
+
 // toggle small or large menu
 $MENU_TOGGLE.on('click', function() {
 		console.log('clicked - menu toggle');
