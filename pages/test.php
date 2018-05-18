ae5cf6962a0fa048e0ed4fa19735d227af0bea51
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-18 18:18:32 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -4,9 +4,9 @@ require('header.php');
 require('navigation.php');
 
 
-$dir = X509_get_default_cert_file();
+//$dir = X509_get_default_cert_file();
 
-var_dump($dir);
+//var_dump($dir);
 
 require('footer.php');
 
