e8a8f6dc0b62fea0a872e12b5768fe868376ec0c
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-18 18:23:07 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -2,11 +2,11 @@
 
 require('header.php');
 require('navigation.php');
-
-
-//$dir = X509_get_default_cert_file();
-
-//var_dump($dir);
+$uuid = "aaa9693e-79dd-4f14-9240-98b30c16b5b2"
+$domName = $lv->domain_get_name_by_uuid($uuid);
+$dom = $lv->get_domain_object($domName);
+$ret = domain_get_memory_stats($domName);
+var_dump($ret);
 
 require('footer.php');
 
