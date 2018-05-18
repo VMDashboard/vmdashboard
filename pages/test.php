fb80574b95b16a39073083f5e9cad90791adfe54
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-18 18:25:27 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -7,8 +7,47 @@ $uuid = "aaa9693e-79dd-4f14-9240-98b30c16b5b2";
 $domName = $lv->domain_get_name_by_uuid($uuid);
 $dom = $lv->get_domain_object($domName);
 $ret = domain_get_memory_stats($domName);
-var_dump($ret);
 
+
+?>
+<!-- page content -->
+<div class="right_col" role="main">
+  <div class="">
+    <div class="page-title">
+      <div class="title_left">
+        <h3>Host</h3>
+      </div>
+    </div>
+  </div>
+
+  <div class="clearfix"></div>
+
+  <div class="row">
+    <div class="col-md-12">
+      <div class="x_panel">
+        <div class="x_title">
+          <h2>Host: <?php echo $hn; ?></h2>
+
+          <div class="clearfix"></div>
+        </div>
+
+        <div class="x_content">
+
+          <div class="col-md-9 col-sm-9 col-xs-12">
+
+          <?php
+          var_dump($ret);
+          ?>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+        <!-- /page content -->
+
+<?php
 require('footer.php');
 
  ?>
