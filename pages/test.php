6525416a46f5fc48c98fe54d1d37b83a60c9e8f4
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-18 18:27:04 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -1,15 +1,14 @@
 <?php
-
 require('header.php');
-require('navigation.php');
 
 $uuid = "aaa9693e-79dd-4f14-9240-98b30c16b5b2";
 $domName = $lv->domain_get_name_by_uuid($uuid);
 $dom = $lv->get_domain_object($domName);
 $ret = domain_get_memory_stats($domName);
 
-
+require('navigation.php');
 ?>
+
 <!-- page content -->
 <div class="right_col" role="main">
   <div class="">
@@ -45,9 +44,8 @@ $ret = domain_get_memory_stats($domName);
     </div>
   </div>
 </div>
-        <!-- /page content -->
+<!-- /page content -->
 
 <?php
 require('footer.php');
-
- ?>
+?>
