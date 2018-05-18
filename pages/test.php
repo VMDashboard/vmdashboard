212964614df2658427b05123b565d7589e113fcb
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-18 18:23:36 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -2,7 +2,8 @@
 
 require('header.php');
 require('navigation.php');
-$uuid = "aaa9693e-79dd-4f14-9240-98b30c16b5b2"
+
+$uuid = "aaa9693e-79dd-4f14-9240-98b30c16b5b2";
 $domName = $lv->domain_get_name_by_uuid($uuid);
 $dom = $lv->get_domain_object($domName);
 $ret = domain_get_memory_stats($domName);
