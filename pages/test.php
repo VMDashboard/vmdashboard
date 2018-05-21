dc995dbf385b8ccd2cd03c9d20d8f08dde2bb477
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-21 19:26:38 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -212,25 +212,10 @@ swal(alertRet);
 
 
 
-      <div class="col-md-8 col-sm-8 col-xs-12" style="overflow-y: scroll;">
+      <div class="col-md-8 col-sm-8 col-xs-12">
         <div class="x_panel tile fixed_height_320" style="overflow-y: scroll;">
           <div class="x_title" style="overflow-y: scroll;">
             <h2><a href="?uuid=<?php echo $uuid; ?>"><?php echo $domName; ?></a></h2>
-            <ul class="nav navbar-right panel_toolbox">
-              <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
-              </li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-wrench"></i></a>
-                <ul class="dropdown-menu" role="menu">
-                  <li><a href="#">Settings 1</a>
-                  </li>
-                  <li><a href="#">Settings 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li><a class="close-link"><i class="fa fa-close"></i></a>
-              </li>
-            </ul>
             <div class="clearfix"></div>
           </div>
           <div class="x_content">
