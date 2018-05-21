5822e8f5359e01a449ea31a2581dc25170407332
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-21 15:00:01 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -245,12 +245,12 @@ swal(alertRet);
 
 
 
-
+<div class="col-md-8 col-sm-8 col-xs-12">
 
 
 
     <div class="row">
-      <div class="col-md-8 col-sm-8 col-xs-12">
+      <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="x_panel">
           <div class="x_title">
             <h2><a href="?uuid=<?php echo $uuid; ?>"><?php echo $domName; ?></a></h2>
