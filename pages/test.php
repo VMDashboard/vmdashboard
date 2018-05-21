f8bbc864c2d15f5a22ddab85d84e1023613f2870
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-21 15:18:32 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -213,7 +213,7 @@ swal(alertRet);
 
 
       <div class="col-md-8 col-sm-8 col-xs-12">
-        <div class="x_panel tile fixed_height_320">
+        <div class="x_panel tile fixed_height_320 overflow_hidden">
           <div class="x_title">
             <h2><a href="?uuid=<?php echo $uuid; ?>"><?php echo $domName; ?></a></h2>
             <ul class="nav navbar-right panel_toolbox">
