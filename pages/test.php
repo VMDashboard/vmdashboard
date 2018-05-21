386ff6dc9a45b8b03096fbd279e0e2442489642f
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-21 15:09:50 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -205,38 +205,7 @@ swal(alertRet);
       </div>
     </div>
 
-    <div class="col-md-4 col-sm-4 col-xs-12">
-      <div class="x_panel tile fixed_height_320">
-        <div class="x_title">
-          <h2>Console</h2>
-          <div class="clearfix"></div>
-        </div>
-        <div class="x_content">
-          <div class="dashboard-widget-content">
-            <ul class="quick-list">
-              <li><i class="fa fa-calendar-o"></i><a href="#">Settings</a></li>
-              <li><i class="fa fa-bars"></i><a href="#">Subscription</a></li>
-              <li><i class="fa fa-bar-chart"></i><a href="#">Auto Renewal</a> </li>
-              <li><i class="fa fa-line-chart"></i><a href="#">Achievements</a></li>
-              <li><i class="fa fa-bar-chart"></i><a href="#">Auto Renewal</a> </li>
-              <li><i class="fa fa-line-chart"></i><a href="#">Achievements</a></li>
-              <li><i class="fa fa-area-chart"></i><a href="#">Logout</a></li>
-            </ul>
 
-            <div class="sidebar-widget">
-              <h4>Profile Completion</h4>
-              <canvas width="150" height="80" id="chart_gauge_01" class="" style="width: 160px; height: 100px;"></canvas>
-              <div class="goal-wrapper">
-                <span id="gauge-text" class="gauge-value pull-left">0</span>
-                <span class="gauge-value pull-left">%</span>
-                <span id="goal-text" class="goal-value pull-right">100%</span>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
-    </div>
 
 
 
@@ -617,6 +586,45 @@ swal(alertRet);
           </div>
         </div>
       </div>
+
+      <div class="col-md-4 col-sm-4 col-xs-12">
+        <div class="x_panel tile fixed_height_320">
+          <div class="x_title">
+            <h2>Console</h2>
+            <div class="clearfix"></div>
+          </div>
+          <div class="x_content">
+            <div class="dashboard-widget-content">
+              <ul class="quick-list">
+                <li><i class="fa fa-calendar-o"></i><a href="#">Settings</a></li>
+                <li><i class="fa fa-bars"></i><a href="#">Subscription</a></li>
+                <li><i class="fa fa-bar-chart"></i><a href="#">Auto Renewal</a> </li>
+                <li><i class="fa fa-line-chart"></i><a href="#">Achievements</a></li>
+                <li><i class="fa fa-bar-chart"></i><a href="#">Auto Renewal</a> </li>
+                <li><i class="fa fa-line-chart"></i><a href="#">Achievements</a></li>
+                <li><i class="fa fa-area-chart"></i><a href="#">Logout</a></li>
+              </ul>
+
+              <div class="sidebar-widget">
+                <h4>Profile Completion</h4>
+                <canvas width="150" height="80" id="chart_gauge_01" class="" style="width: 160px; height: 100px;"></canvas>
+                <div class="goal-wrapper">
+                  <span id="gauge-text" class="gauge-value pull-left">0</span>
+                  <span class="gauge-value pull-left">%</span>
+                  <span id="goal-text" class="goal-value pull-right">100%</span>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
     </div>
   </div>
 </div>
