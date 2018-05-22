02f0ac60fde6b31d6e0e9c40c3c6fd9ab334e8da
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-22 13:30:15 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -262,8 +262,8 @@ function domainDeleteWarning(linkURL) {
             /* General information */
             echo "<table class=\"table\">";
               echo "<tr>";
-                echo "<td style=\"background: #f3f3f3; padding:5px; line-height:1; \"><strong>Type: </strong></td>";
-                echo "<td style=\"padding:5px; line-height:1; \">" . $lv->get_domain_type($domName) . "</td>";
+                echo "<td style=\"background: #f3f3f3; line-height:1; \"><strong>Type: </strong></td>";
+                echo "<td style=\"line-height:1; \">" . $lv->get_domain_type($domName) . "</td>";
               echo "</tr>";
 
               echo "<tr>";
