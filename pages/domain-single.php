47fe9fa88ee26a89acca2e5bde63bc6edd23aed4
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-22 13:47:10 -0400

    commit

--- pages/domain-single.php
+++ pages/domain-single.php
@@ -253,7 +253,7 @@ function domainDeleteWarning(linkURL) {
       <div class="col-md-4 col-sm-4 col-xs-12">
         <div class="x_panel tile fixed_height_320">
           <div class="x_title">
-            <h2><a href="?uuid=<?php echo $uuid; ?>"><?php echo $domName; ?></a></h2>
+            <h2><a href="?uuid=<?php echo $uuid; ?>">Domain Name: <?php echo $domName; ?></a></h2>
             <div class="clearfix"></div>
           </div>
           <div class="x_content">
