a163f034a2abfdb198c36494d35ca70f52c5568a
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-21 20:10:40 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -560,7 +560,7 @@ swal(alertRet);
 
 
 
-      <div class="col-md-4 col-sm-4 col-xs-12">
+      <div class="col-md-8 col-sm-8 col-xs-12">
         <div class="x_panel tile">
           <div class="x_title">
             <h2>Actions</h2>
