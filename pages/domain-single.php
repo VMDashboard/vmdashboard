a2f8061eb28f0009ade795ee186127244a89409f
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-22 13:43:24 -0400

    commit

--- pages/domain-single.php
+++ pages/domain-single.php
@@ -1,16 +1,38 @@
 <?php
+// If the SESSION has not been started, start it now
+if (!isset($_SESSION)) {
+    session_start();
+}
+
+// If there is no username, then we need to send them to the login
+if (!isset($_SESSION['username'])){
+  header('Location: login.php');
+}
+
+// We are now going to grab any GET/POST data and put in in SESSION data, then clear it.
+// This will prevent duplicatig actions when page is reloaded.
+if (isset($_GET['action']) || isset($_GET['dev']) || isset($_GET['mac']) || isset($_GET['snapshot'])) {
+    $_SESSION['action'] = $_GET['action'];
+    $_SESSION['dev'] = $_GET['dev'];
+    $_SESSION['mac'] = $_GET['mac'];
+    $_SESSION['snapshot'] = $_GET['snapshot'];
+    $_SESSION['xmldesc'] = $_POST['xmldesc'];
+    header("Location: ".$_SERVER['PHP_SELF']."?uuid=".$_GET['uuid']);
+    exit;
+}
+
+// Add the header information
 require('header.php');
 
+//Set variables
 $uuid = $_GET['uuid'];
-$domName = $lv->domain_get_name_by_uuid($_GET['uuid']);
+$domName = $lv->domain_get_name_by_uuid($uuid);
 $dom = $lv->get_domain_object($domName);
 $protocol = isset($_SERVER['HTTPS']) ? "https://" : "http://";
 $url = $protocol . $_SERVER['HTTP_HOST'];
 $page = basename($_SERVER['PHP_SELF']);
-$action = $_GET['action'];
-$domXML = $lv->domain_get_xml($domName);
-$domXML = new SimpleXMLElement($domXML);
-//$domXML = new SimpleXMLElement($lv->domain_get_xml($domName));
+$action = $_SESSION['action'];
+$domXML = new SimpleXMLElement($lv->domain_get_xml($domName));
 
 
 // Domain Actions
@@ -31,7 +53,7 @@ if ($action == 'domain-stop') {
   $actioninfo = $lv->domain_get_info($dom);
   $actionstate = $lv->domain_state_translate($actioninfo['state']);
   if ($actionstate == "running"){
-    $ret = "Domain is unable to shutdown gracefully. It will need to be forcefully turned off";
+    $ret = "Domain is unable to shutdown gracefully. It may need to be forcefully shutdown";
   }
 }
 
@@ -48,8 +70,8 @@ if ($action == 'domain-delete') {
 
 //Disk Actions
 if ($action == 'domain-disk-remove') {
-  $dev = $_GET['dev'];
-  //My XML way
+  $dev = $_SESSION['dev'];
+  //Using XML to remove a disk, $ret = $lv->domain_disk_remove($domName, $dev) was not working correctly
   $path = $domXML->xpath('//disk');
   for ($i = 0; $i < sizeof($path); $i++) {
     if ($domXML->devices->disk[$i]->target[dev] == $dev)
@@ -58,15 +80,13 @@ if ($action == 'domain-disk-remove') {
       $newXML = str_replace('<?xml version="1.0"?>', '', $newXML);
       $ret = $lv->domain_change_xml($domName, $newXML) ? 'Disk has been removed successfully' : 'Cannot remove disk: '.$lv->get_last_error();
   }
-
-  //Suggested way, however not working
-  //$ret = $lv->domain_disk_remove($domName, $dev) ? 'Disk has been removed successfully' : 'Cannot remove disk: '.$lv->get_last_error();
 }
+
 
 //Network Actions
 if ($action == 'domain-nic-remove') {
-  $mac = base64_decode($_GET['mac']);
-  //My XML way
+  $mac = base64_decode($_SESSION['mac']);
+  //Using XML to remove network, $ret = $lv->domain_nic_remove($domName, $mac) was not working correctly
   $path = $domXML->xpath('//interface');
   for ($i = 0; $i < sizeof($path); $i++) {
     if ($domXML->devices->interface[$i]->mac[address] == $mac)
@@ -75,10 +95,8 @@ if ($action == 'domain-nic-remove') {
       $newXML = str_replace('<?xml version="1.0"?>', '', $newXML);
       $ret = $lv->domain_change_xml($domName, $newXML) ? 'Network interface has been removed successfully' : 'Cannot remove network interface: '.$lv->get_last_error();
   }
-
-  //Suggested way, however not working
-  //$ret = $lv->domain_nic_remove($domName, $mac) ? "Network device successfully removed" : 'Error while removing network device: '.$lv->get_last_error();
 }
+
 
 //Snapshot Actions
 if ($action == 'domain-snapshot-create') {
@@ -86,17 +104,17 @@ if ($action == 'domain-snapshot-create') {
 }
 
 if ($action == 'domain-snapshot-delete') {
-  $snapshot = $_GET['snapshot'];
+  $snapshot = $_SESSION['snapshot'];
   $ret = $lv->domain_snapshot_delete($domName, $snapshot) ? "Snapshot $snapshot for $domName successfully deleted" : 'Error while deleting snapshot of domain: '.$lv->get_last_error();
 }
 
 if ($action == 'domain-snapshot-revert') {
-  $snapshot = $_GET['snapshot'];
+  $snapshot = $_SESSION['snapshot'];
   $ret = $lv->domain_snapshot_revert($domName, $snapshot) ? "Snapshot $snapshot for $domName successfully applied" : 'Error while reverting snapshot of domain: '.$lv->get_last_error();
 }
 
 if ($action == 'domain-snapshot-xml') {
-  $snapshot = $_GET['snapshot'];
+  $snapshot = $_SESSION['snapshot'];
   $snapshotxml = $lv->domain_snapshot_get_xml($domName, $snapshot);
   //Parsing the snapshot XML file - in Ubuntu requires the php-xml package
   $xml = simplexml_load_string($snapshotxml);
@@ -105,12 +123,14 @@ if ($action == 'domain-snapshot-xml') {
 }
 
 if ($action == 'domain-edit') {
-  $xml = $_POST['xmldesc'];
+  $xml = $_SESSION['xmldesc'];
     $ret = $lv->domain_change_xml($domName, $xml) ? "Domain definition has been changed" : 'Error changing domain definition: '.$lv->get_last_error();
 }
 
+
 //get info, mem, cpu, state, id, arch, and vnc after actions to reflect any changes to domain
-$info = $lv->domain_get_info($dom);
+//Didn't use $info = $lv->domain_get_info($dom); because of caches state.
+$info = libvirt_domain_get_info($dom);
 $mem = number_format($info['memory'] / 1048576, 2, '.', ' ').' GB';
 $cpu = $info['nrVirtCpu'];
 $state = $lv->domain_state_translate($info['state']);
@@ -132,7 +152,7 @@ $listarray = $lv->get_domains();
 foreach ($listarray as $listname) {
   $listdom = $lv->get_domain_object($listname);
   $listinfo = libvirt_domain_get_info($listdom);
-  //Don't use $lv->domain_get_info($listdom) because the state is cached and caused delay state status
+  //Don't use $lv->domain_get_info($listdom) because the state is cached and caused delayed state status
   $liststate = $lv->domain_state_translate($listinfo['state']);
   if ($liststate == "running") {
     $listdomuuid = libvirt_domain_get_uuid_string($listdom);
@@ -142,18 +162,40 @@ foreach ($listarray as $listname) {
 }
 $listfile = "../tokens.list";
 $list = file_put_contents($listfile, $liststring);
+
+
+//Remove session variables so that if page reloads it will not perform actions again
+unset($_SESSION['action']);
+unset($_SESSION['dev']);
+unset($_SESSION['mac']);
+unset($_SESSION['snapshot']);
+unset($_SESSION['xmldesc']);
+
+
+if ($ret) {
+  ?>
+  <script>
+    var alertRet = "<?php echo $ret; ?>";
+    swal(alertRet);
+  </script>
+  <?php
+}
+
 ?>
 
-<?php
-if ($ret) {
-?>
 <script>
-var alertRet = "<?php echo $ret; ?>";
-swal(alertRet);
+function domainDeleteWarning(linkURL) {
+  swal("Are you sure you want to delete the domain?", {
+    buttons: ["Cancel", true],
+  }).then((value) => {
+    if (value == true){
+    // Redirect the user
+    window.location = linkURL;
+  }
+
+  });
+  }
 </script>
-<?php
-}
-?>
 
 
 <!-- page content -->
@@ -182,7 +224,7 @@ swal(alertRet);
                 <?php
                 if ($state == "running") {
                   //screenshot will get raw png data at 300 pixels wide
-                  $screenshot = $lv->domain_get_screenshot_thumbnail($_GET['uuid'], 400);
+                  $screenshot = $lv->domain_get_screenshot_thumbnail($uuid, 400);
                   //the raw png data needs to be encoded to use with html img tag
                   $screen64 = base64_encode($screenshot['data']);
                   ?>
@@ -209,7 +251,7 @@ swal(alertRet);
 
 
       <div class="col-md-4 col-sm-4 col-xs-12">
-        <div class="x_panel tile fixed_height_320" style="overflow-y: scroll;">
+        <div class="x_panel tile fixed_height_320">
           <div class="x_title">
             <h2><a href="?uuid=<?php echo $uuid; ?>"><?php echo $domName; ?></a></h2>
             <div class="clearfix"></div>
@@ -218,14 +260,49 @@ swal(alertRet);
 
             <?php
             /* General information */
-            echo "<strong>Domain type: </strong>".$lv->get_domain_type($domName)."<br />";
-            echo "<strong>Domain emulator: </strong>".$lv->get_domain_emulator($domName)."<br />";
-            echo "<strong>Domain memory: </strong>$mem<br />";
-            echo "<strong>Number of vCPUs: </strong>$cpu<br />";
-            echo "<strong>Domain state: </strong>$state<br />";
-            echo "<strong>Domain architecture: </strong>$arch<br />";
-            echo "<strong>Domain ID: </strong>$id<br />";
-            echo "<strong>VNC Port: </strong>$vnc<br />";
+            echo "<table class=\"table\">";
+              echo "<tr>";
+                echo "<td style=\"background: #f3f3f3; line-height:1; border-top:none;\"><strong>Type: </strong></td>";
+                echo "<td style=\"line-height:1; border-top:none;\">" . $lv->get_domain_type($domName) . "</td>";
+              echo "</tr>";
+
+              echo "<tr>";
+                echo "<td style=\"background: #f3f3f3; line-height:1;\"><strong>Emulator: </strong></td>";
+                echo "<td style=\"line-height:1; \">" . $lv->get_domain_emulator($domName) . "</td>";
+              echo "</tr>";
+
+              echo "<tr>";
+                echo "<td style=\"background: #f3f3f3; line-height:1;\"><strong>Memory: </strong></td>";
+                echo "<td style=\"line-height:1; \">" . $mem . "</td>";
+              echo "</tr>";
+
+              echo "<tr>";
+                echo "<td style=\"background: #f3f3f3; line-height:1;\"><strong>vCPUs: </strong></td>";
+                echo "<td style=\"line-height:1; \">" . $cpu . "</td>";
+              echo "</tr>";
+
+              echo "<tr>";
+                echo "<td style=\"background: #f3f3f3; line-height:1;\"><strong>State: </strong></td>";
+                echo "<td style=\"line-height:1; \">" . $state . "</td>";
+              echo "</tr>";
+
+              echo "<tr>";
+                echo "<td style=\"background: #f3f3f3; line-height:1;\"><strong>Architecture: </strong></td>";
+                echo "<td style=\"line-height:1; \">" . $arch . "</td>";
+              echo "</tr>";
+
+              echo "<tr style=\"line-height:1;\">";
+                echo "<td style=\"background: #f3f3f3; line-height:1;\"><strong>ID: </strong></td>";
+                echo "<td style=\"line-height:1; \">" . $id . "</td>";
+              echo "</tr>";
+
+              echo "<tr>";
+                echo "<td style=\"background: #f3f3f3; line-height:1;\"><strong>VNC Port: </strong></td>";
+                echo "<td style=\"line-height:1; \">" . $vnc . "</td>";
+              echo "</tr>";
+
+            echo "</table>";
+
             if ($die)
               die('</body></html');
               ?>
@@ -281,7 +358,7 @@ swal(alertRet);
                 <?php } ?>
 
                 <?php  if ($state == "shutoff") { ?>
-                  <li><a href="?action=domain-delete&amp;uuid=<?php echo $uuid; ?>" target="_self" >
+                  <li><a onclick="domainDeleteWarning('?action=domain-delete&amp;uuid=<?php echo $uuid; ?>')" href="#" >
                     <i class="fa fa-trash"></i> Delete domain <br />
                   </a></li>
                 <?php } ?>
@@ -289,15 +366,15 @@ swal(alertRet);
                 <hr>
 
                 <li><a href="domain-add-disk.php?action=domain-disk-add&amp;uuid=<?php echo $uuid; ?>" target="_self" >
-                  <i class="fa fa-plus"></i> Add new disk<br />
+                  <i class="fa fa-plus"></i> Add storage volume<br />
                 </a></li>
 
                 <li><a href="domain-add-iso.php?action=domain-disk-add&amp;uuid=<?php echo $uuid; ?>" target="_self" >
-                  <i class="fa fa-plus"></i> Add new iso image<br />
+                  <i class="fa fa-plus"></i> Add optical storage<br />
                 </a></li>
 
                 <li><a href="domain-add-network.php?uuid=<?php echo $uuid; ?>" target="_self" >
-                  <i class="fa fa-plus"></i> Add new network<br />
+                  <i class="fa fa-plus"></i> Add network adapter<br />
                 </a></li>
 
                 <li><a href="?action=domain-snapshot-create&amp;uuid=<?php echo $uuid; ?>" target="_self" >
@@ -311,7 +388,6 @@ swal(alertRet);
           </div>
         </div>
       </div>
-
 
 
 
@@ -503,12 +579,6 @@ swal(alertRet);
 
 
 
-
-
-
-
-
-
       <div class="col-md-6 col-sm-6 col-xs-12">
         <div class="x_panel tile fixed_height_320" style="overflow-y: scroll;">
           <div class="x_title">
@@ -544,8 +614,8 @@ swal(alertRet);
                 echo "<td>" . $snapstate . "</td>";
                 echo "<td>
                   <a title='Delete snapshot' href=\"?action=domain-snapshot-delete&amp;snapshot=$value&amp;uuid=$uuid\">Delete | </a>
-                  <a title='Revert snapshot' href=?action=domain-snapshot-revert&amp;uuid=" . $_GET['uuid'] . "&amp;snapshot=" . $value . ">Revert | </a>
-                  <a title='View snapshot XML' href=?action=domain-snapshot-xml&amp;uuid=" . $_GET['uuid'] . "&amp;snapshot=" . $value . ">XML</a>
+                  <a title='Revert snapshot' href=?action=domain-snapshot-revert&amp;uuid=" . $uuid . "&amp;snapshot=" . $value . ">Revert | </a>
+                  <a title='View snapshot XML' href=?action=domain-snapshot-xml&amp;uuid=" . $uuid . "&amp;snapshot=" . $value . ">XML</a>
                   </td>";
                 echo "</tr>";
               }
@@ -570,7 +640,6 @@ swal(alertRet);
 
 
 
-
       <div class="col-md-6 col-sm-6 col-xs-12">
         <div class="x_panel tile">
           <div class="x_title">
@@ -586,7 +655,7 @@ swal(alertRet);
 
 
             if ($state == "shutoff"){
-              $ret = "<form method=\"POST\" action=?action=domain-edit&amp;uuid=" . $_GET['uuid'] . " >" .
+              $ret = "<form method=\"POST\" action=?action=domain-edit&amp;uuid=" . $uuid . " >" .
                 "<textarea name=\"xmldesc\" rows=\"17\" style=\"width: 100%; margin: 0; padding: 0; border-width: 0; background-color:#ebecf1;\" >" . $xml . "</textarea>" .
                 "<br /> <br /> <input type=\"submit\" value=\"Save XML\"></form>";
               echo $ret;
