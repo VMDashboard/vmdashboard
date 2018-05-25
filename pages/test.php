9a64a6c5e7b38bdd86134c23274421df18eed36d
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-24 22:39:38 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -240,8 +240,8 @@ function domainDeleteWarning(linkURL) {
             <div class="bs-glyphicons">
               <ul class="bs-glyphicons-list">
                 <?php  if ($state == "running") { ?>
-                  <li style="width:25%; height:50px;"><a href="<?php echo $url; ?>:6080/vnc.html?path=?token=<?php echo $uuid; ?>" target="_blank" >
-                    <i class="fa fa-desktop" style="font-size:25px;"></i><br /> noVNC<br />
+                  <li style="width:25%; height:75px;"><a href="<?php echo $url; ?>:6080/vnc.html?path=?token=<?php echo $uuid; ?>" target="_blank" >
+                    <i class="fa fa-desktop" style="font-size:20px;"></i><br /> noVNC<br />
                   </a></li>
                 <?php } ?>
 
