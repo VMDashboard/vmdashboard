a899f747be2725be187f810c938de255f344cd8a
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-24 21:41:14 -0400

    commit

--- pages/test.php
+++ pages/test.php
@@ -243,7 +243,7 @@ function domainDeleteWarning(linkURL) {
                       <div class="bs-glyphicons">
                         <ul class="bs-glyphicons-list">
 
-                          <li>
+                          <li width="25%">
                             <span class="glyphicon glyphicon-asterisk" aria-hidden="true"></span>
                             <span class="glyphicon-class">glyphicon glyphicon-asterisk</span>
                           </li>
