b5ebcb31c2aa91c022ba2724afa6df317a9bb965
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-29 11:13:55 -0400

    commit

--- pages/navigation.php
+++ pages/navigation.php
@@ -82,7 +82,7 @@ if (!isset($_SESSION)) {
         <div class="nav_menu">
           <nav>
             <div class="nav toggle">
-              <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              <a style="color: #5A738E;" id="menu_toggle"><i class="fa fa-bars"></i></a>
             </div>
 
             <ul class="nav navbar-nav navbar-right">
