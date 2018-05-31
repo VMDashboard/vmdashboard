98cb1289caf9d8bc1b6574cb0998f01cb7a0467c
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-31 12:26:51 -0400

    commit

--- pages/domain-single.php
+++ pages/domain-single.php
@@ -314,7 +314,7 @@ function domainDeleteWarning(linkURL) {
                 <?php  if ($state == "running") { ?>
                   <li><i class="fa fa-desktop"></i>
                     <a href="<?php echo $url; ?>:6080/vnc.html?path=?token=<?php echo $uuid; ?>" target="_blank" >
-                     Connect using noVNC<br /></a>
+                     &nbsp;Connect using noVNC<br /></a>
                   </li>
                 <?php } ?>
 
