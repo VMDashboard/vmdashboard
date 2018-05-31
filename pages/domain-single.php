11d18357e6b8c7a2dac22c44089e556b98aa645b
matthewalanpenning <matthewalanpenning@gmail.com>
2018-05-31 12:29:48 -0400

    commit

--- pages/domain-single.php
+++ pages/domain-single.php
@@ -314,32 +314,32 @@ function domainDeleteWarning(linkURL) {
                 <?php  if ($state == "running") { ?>
                   <li><i class="fa fa-desktop"></i>
                     <a href="<?php echo $url; ?>:6080/vnc.html?path=?token=<?php echo $uuid; ?>" target="_blank" >
-                     &nbsp;&nbsp;Connect using noVNC<br /></a>
+                    &nbsp;&nbsp;&nbsp;Connect using noVNC<br /></a>
                   </li>
                 <?php } ?>
 
                 <?php if ($state == "shutoff") { ?>
                   <li><i class="fa fa-power-off"></i>
                     <a href="?action=domain-start&amp;uuid=<?php echo $uuid; ?>" target="_self" >
-                    &nbsp;&nbsp;Power on<br /></a>
+                    &nbsp;&nbsp;&nbsp;Power on<br /></a>
                   </li>
                 <?php } ?>
 
                 <?php  if ($state == "running") { ?>
                   <li><i class="fa fa-power-off"></i>
                     <a href="?action=domain-stop&amp;uuid=<?php echo $uuid; ?>" target="_self" >
-                     &nbsp;&nbsp;Shutdown<br /></a>
+                    &nbsp;&nbsp;&nbsp;Shutdown<br /></a>
                   </li>
                   <li><i class="fa fa-pause"></i>
                     <a href="?action=domain-pause&amp;uuid=<?php echo $uuid; ?>" target="_self" >
-                     &nbsp;&nbsp;Pause domain <br /></a>
+                    &nbsp;&nbsp;&nbsp;Pause domain <br /></a>
                   </li>
                 <?php } ?>
 
                 <?php  if ($state == "paused") { ?>
                   <li><i class="fa fa-play"></i>
                     <a href="?action=domain-resume&amp;uuid=<?php echo $uuid; ?>" target="_self" >
-                    &nbsp;&nbsp;Resume domain <br /></a>
+                    &nbsp;&nbsp;&nbsp;Resume domain <br /></a>
                   </li>
                 <?php } ?>
 
