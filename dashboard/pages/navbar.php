9e4e9f76ef23ca18d6765cf05d77f0920756d13c
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-02 22:30:09 -0400

    commit

--- dashboard/pages/navbar.php
+++ dashboard/pages/navbar.php
@@ -97,7 +97,7 @@
                 <span class="navbar-toggler-bar bar3"></span>
               </button>
             </div>
-            <a class="navbar-brand" href="#pablo">Dashboard</a>
+            <a class="navbar-brand" href="#">Dashboard</a>
           </div>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-bar navbar-kebab"></span>
