1f520022c65a502ec3c0bf167d19ac21b175a0fb
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-02 22:32:37 -0400

    commit

--- dashboard/pages/header.php
+++ dashboard/pages/header.php
@@ -2,8 +2,8 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/sqaurelogo.png">
-    <link rel="icon" type="image/png" href="../assets/img/squarelogo.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/sqaurelogo.png">
+    <link rel="icon" type="image/png" href="../../assets/img/squarelogo.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>
       openVM
@@ -13,6 +13,6 @@
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     <!-- CSS Files -->
-    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="../assets/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
+    <link href="../../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../../assets/css/paper-dashboard.css?v=2.0.1" rel="stylesheet" />
   </head>
