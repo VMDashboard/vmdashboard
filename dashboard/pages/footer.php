702828d8b80376a771c5a762e9ae8661686cb23c
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-02 22:37:20 -0400

    commit

--- dashboard/pages/footer.php
+++ dashboard/pages/footer.php
@@ -3,14 +3,12 @@
             <div class="row">
               <nav class="footer-nav">
                   <ul>
-                      <li><a href="http://www.creative-tim.com">Creative Tim</a></li>
-                      <li><a href="http://blog.creative-tim.com">Blog</a></li>
-                      <li><a href="https://www.creative-tim.com/license">Licenses</a></li>
+                      <li><a href="https://openvm.tech">openVM.tech</a></li>
                   </ul>
               </nav>
               <div class="credits ml-auto">
                   <span class="copyright">
-                      © <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Creative Tim
+                      © <script>document.write(new Date().getFullYear())</script>, made with <i class="fa fa-heart heart"></i> by Penning Development
                   </span>
               </div>
             </div>
