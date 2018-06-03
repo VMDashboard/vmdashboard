d6705b0dece490ac33dc41d992e4aa99d4b5571e
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-02 21:49:34 -0400

    commit

--- dashboard/pages/header.php
+++ dashboard/pages/header.php
@@ -2,11 +2,11 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-    <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/sqaurelogo.png">
+    <link rel="icon" type="image/png" href="../assets/img/squarelogo.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>
-      Paper Dashboard 2 PRO by Creative Tim
+      openVM
     </title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
     <!--     Fonts and icons     -->
