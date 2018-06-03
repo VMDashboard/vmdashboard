024217e6e9b64cd9eb0677dbc318f1240cf6ac50
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-02 21:54:11 -0400

    commit

--- dashboard/pages/navbar.php
+++ dashboard/pages/navbar.php
@@ -1,125 +1,310 @@
-  <body>
-    <div class="wrapper">
-      <div class="sidebar">
+<body class="">
+  <div class="wrapper ">
+    <div class="sidebar" data-color="white" data-active-color="danger">
       <!--
-          Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
-      -->
-
-        <div class="logo">
-          <a href="http://www.creative-tim.com" class="simple-text logo-mini">
-              CT
-          </a>
-
-          <a href="http://www.creative-tim.com" class="simple-text logo-normal">
-              Creative Tim
-          </a>
-
-
-        <div class="sidebar-wrapper">
-          <div class="user">
-              <div class="photo">
-                  <img src="../assets/img/james.jpg" />
-              </div>
-              <div class="info">
-                  <a data-toggle="collapse" href="#collapseExample" class="collapsed">
-                      <span>
-                          Chet Faker
-                          <b class="caret"></b>
-                      </span>
-                  </a>
-                  <div class="clearfix"></div>
-                  <div class="collapse" id="collapseExample">
-                      <ul class="nav">
-                          <li>
-                              <a href="#">
-                                  <span class="sidebar-mini-icon">MP</span>
-                                  <span class="sidebar-normal">My Profile</span>
-                              </a>
-                          </li>
-                          <li>
-                              <a href="#">
-                                  <span class="sidebar-mini-icon">EP</span>
-                                  <span class="sidebar-normal">Edit Profile</span>
-                              </a>
-                          </li>
-                          <li>
-                              <a href="#">
-                                  <span class="sidebar-mini-icon">S</span>
-                                  <span class="sidebar-normal">Settings</span>
-                              </a>
-                          </li>
-                      </ul>
-                  </div>
-              </div>
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+    -->
+      <div class="logo">
+        <a href="#" class="simple-text logo-mini">
+          <div class="logo-image-small">
+            <img src="../assets/img/squarelogo.png">
           </div>
-          <ul class="nav">
-
-              <li class="active">
-                  <a href="../examples/dashboard.html">
-                      <i class="nc-icon nc-layout-11"></i>
-                      <p>Example 1</p>
-                  </a>
-              </li>
-
-              <li>
-                  <a data-toggle="collapse" href="#pagesExamples">
-                      <i class="nc-icon nc-laptop"></i>
-                      <p>Example 2
-                         <b class="caret"></b>
-                      </p>
-                  </a>
-
-                  <div class="collapse" id="pagesExamples">
-                      <ul class="nav">
-                          <li>
-                              <a href="../examples/pages/pricing.html">
-                                  <span class="sidebar-mini-icon">C1</span>
-                                  <span class="sidebar-normal">Collapse 1</span>
-                              </a>
-                          </li>
-                          <li>
-                              <a href="../examples/pages/timeline.html">
-                                  <span class="sidebar-mini-icon">C2</span>
-                                  <span class="sidebar-normal">Collapse 2</span>
-                              </a>
-                          </li>
-                      </ul>
-                  </div>
-              </li>
-          </ul>
-        </div>
+        </a>
+        <a href="#" class="simple-text logo-normal">
+          openVM - Dashboard
+          <!-- <div class="logo-image-big">
+            <img src="../assets/img/logo-big.png">
+          </div> -->
+        </a>
       </div>
-
-      <div class="main-panel">
-        <nav class="navbar navbar-expand-lg navbar-absolute bg-white fixed-top">
-          <div class="container-fluid">
-            <div class="navbar-wrapper">
-
-              <div class="navbar-minimize">
-                <button id="minimizeSidebar" class="btn btn-icon btn-round">
-                  <i class="nc-icon nc-minimal-right text-center visible-on-sidebar-mini"></i>
-                  <i class="nc-icon nc-minimal-left text-center visible-on-sidebar-regular"></i>
-                </button>
-              </div>
-
-              <div class="navbar-toggle">
-                <button type="button" class="navbar-toggler">
-                  <span class="navbar-toggler-bar bar1"></span>
-                  <span class="navbar-toggler-bar bar2"></span>
-                  <span class="navbar-toggler-bar bar3"></span>
-                </button>
-              </div>
-              <a class="navbar-brand" href="#pablo">Dashboard</a>
+      <div class="sidebar-wrapper">
+        <div class="user">
+          <div class="photo">
+            <img src="../assets/img/faces/ayo-ogunseinde-2.jpg" />
+          </div>
+          <div class="info">
+            <a data-toggle="collapse" href="#collapseExample" class="collapsed">
+              <span>
+                Chet Faker
+                <b class="caret"></b>
+              </span>
+            </a>
+            <div class="clearfix"></div>
+            <div class="collapse" id="collapseExample">
+              <ul class="nav">
+                <li>
+                  <a href="#">
+                    <span class="sidebar-mini-icon">MP</span>
+                    <span class="sidebar-normal">My Profile</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="sidebar-mini-icon">EP</span>
+                    <span class="sidebar-normal">Edit Profile</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <span class="sidebar-mini-icon">S</span>
+                    <span class="sidebar-normal">Settings</span>
+                  </a>
+                </li>
+              </ul>
             </div>
-
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-bar navbar-kebab"></span>
-              <span class="navbar-toggler-bar navbar-kebab"></span>
-              <span class="navbar-toggler-bar navbar-kebab"></span>
-            </button>
-
-            <div class="collapse navbar-collapse justify-content-end" id="navigation">
-
+          </div>
+        </div>
+        <ul class="nav">
+          <li class="active">
+            <a href="../examples/dashboard.html">
+              <i class="nc-icon nc-bank"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li>
+            <a data-toggle="collapse" href="#pagesExamples">
+              <i class="nc-icon nc-book-bookmark"></i>
+              <p>
+                Pages
+                <b class="caret"></b>
+              </p>
+            </a>
+            <div class="collapse " id="pagesExamples">
+              <ul class="nav">
+                <li>
+                  <a href="../examples/pages/timeline.html">
+                    <span class="sidebar-mini-icon">T</span>
+                    <span class="sidebar-normal"> Timeline </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/pages/login.html">
+                    <span class="sidebar-mini-icon">L</span>
+                    <span class="sidebar-normal"> Login </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/pages/register.html">
+                    <span class="sidebar-mini-icon">R</span>
+                    <span class="sidebar-normal"> Register </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/pages/lock.html">
+                    <span class="sidebar-mini-icon">LS</span>
+                    <span class="sidebar-normal"> Lock Screen </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/pages/user.html">
+                    <span class="sidebar-mini-icon">UP</span>
+                    <span class="sidebar-normal"> User Profile </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <a data-toggle="collapse" href="#componentsExamples">
+              <i class="nc-icon nc-layout-11"></i>
+              <p>
+                Components
+                <b class="caret"></b>
+              </p>
+            </a>
+            <div class="collapse " id="componentsExamples">
+              <ul class="nav">
+                <li>
+                  <a href="../examples/components/buttons.html">
+                    <span class="sidebar-mini-icon">B</span>
+                    <span class="sidebar-normal"> Buttons </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/components/grid.html">
+                    <span class="sidebar-mini-icon">G</span>
+                    <span class="sidebar-normal"> Grid System </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/components/panels.html">
+                    <span class="sidebar-mini-icon">P</span>
+                    <span class="sidebar-normal"> Panels </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/components/sweet-alert.html">
+                    <span class="sidebar-mini-icon">SA</span>
+                    <span class="sidebar-normal"> Sweet Alert </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/components/notifications.html">
+                    <span class="sidebar-mini-icon">N</span>
+                    <span class="sidebar-normal"> Notifications </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/components/icons.html">
+                    <span class="sidebar-mini-icon">I</span>
+                    <span class="sidebar-normal"> Icons </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/components/typography.html">
+                    <span class="sidebar-mini-icon">T</span>
+                    <span class="sidebar-normal"> Typography </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <a data-toggle="collapse" href="#formsExamples">
+              <i class="nc-icon nc-ruler-pencil"></i>
+              <p>
+                Forms
+                <b class="caret"></b>
+              </p>
+            </a>
+            <div class="collapse " id="formsExamples">
+              <ul class="nav">
+                <li>
+                  <a href="../examples/forms/regular.html">
+                    <span class="sidebar-mini-icon">RF</span>
+                    <span class="sidebar-normal"> Regular Forms </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/forms/extended.html">
+                    <span class="sidebar-mini-icon">EF</span>
+                    <span class="sidebar-normal"> Extended Forms </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/forms/validation.html">
+                    <span class="sidebar-mini-icon">V</span>
+                    <span class="sidebar-normal"> Validation Forms </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/forms/wizard.html">
+                    <span class="sidebar-mini-icon">W</span>
+                    <span class="sidebar-normal"> Wizard </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <a data-toggle="collapse" href="#tablesExamples">
+              <i class="nc-icon nc-single-copy-04"></i>
+              <p>
+                Tables
+                <b class="caret"></b>
+              </p>
+            </a>
+            <div class="collapse " id="tablesExamples">
+              <ul class="nav">
+                <li>
+                  <a href="../examples/tables/regular.html">
+                    <span class="sidebar-mini-icon">RT</span>
+                    <span class="sidebar-normal"> Regular Tables </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/tables/extended.html">
+                    <span class="sidebar-mini-icon">ET</span>
+                    <span class="sidebar-normal"> Extended Tables </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/tables/datatables.net.html">
+                    <span class="sidebar-mini-icon">DT</span>
+                    <span class="sidebar-normal"> DataTables.net </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <a data-toggle="collapse" href="#mapsExamples">
+              <i class="nc-icon nc-pin-3"></i>
+              <p>
+                Maps
+                <b class="caret"></b>
+              </p>
+            </a>
+            <div class="collapse " id="mapsExamples">
+              <ul class="nav">
+                <li>
+                  <a href="../examples/maps/google.html">
+                    <span class="sidebar-mini-icon">GM</span>
+                    <span class="sidebar-normal"> Google Maps </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/maps/fullscreen.html">
+                    <span class="sidebar-mini-icon">FSM</span>
+                    <span class="sidebar-normal"> Full Screen Map </span>
+                  </a>
+                </li>
+                <li>
+                  <a href="../examples/maps/vector.html">
+                    <span class="sidebar-mini-icon">VM</span>
+                    <span class="sidebar-normal"> Vector Map </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li>
+            <a href="../examples/widgets.html">
+              <i class="nc-icon nc-box"></i>
+              <p>Widgets</p>
+            </a>
+          </li>
+          <li>
+            <a href="../examples/charts.html">
+              <i class="nc-icon nc-chart-bar-32"></i>
+              <p>Charts</p>
+            </a>
+          </li>
+          <li>
+            <a href="../examples/calendar.html">
+              <i class="nc-icon nc-calendar-60"></i>
+              <p>Calendar</p>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="main-panel">
+      <!-- Navbar -->
+      <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
+        <div class="container-fluid">
+          <div class="navbar-wrapper">
+            <div class="navbar-minimize">
+              <button id="minimizeSidebar" class="btn btn-icon btn-round">
+                <i class="nc-icon nc-minimal-right text-center visible-on-sidebar-mini"></i>
+                <i class="nc-icon nc-minimal-left text-center visible-on-sidebar-regular"></i>
+              </button>
+            </div>
+            <div class="navbar-toggle">
+              <button type="button" class="navbar-toggler">
+                <span class="navbar-toggler-bar bar1"></span>
+                <span class="navbar-toggler-bar bar2"></span>
+                <span class="navbar-toggler-bar bar3"></span>
+              </button>
+            </div>
+            <a class="navbar-brand" href="#pablo">Paper Dashboard 2 PRO</a>
+          </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+            <span class="navbar-toggler-bar navbar-kebab"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navigation">
             <form>
               <div class="input-group no-border">
                 <input type="text" value="" class="form-control" placeholder="Search...">
@@ -130,7 +315,6 @@
                 </div>
               </div>
             </form>
-
             <ul class="navbar-nav">
               <li class="nav-item">
                 <a class="nav-link btn-magnify" href="#pablo">
@@ -153,7 +337,6 @@
                   <a class="dropdown-item" href="#">Something else here</a>
                 </div>
               </li>
-
               <li class="nav-item">
                 <a class="nav-link btn-rotate" href="#pablo">
                   <i class="nc-icon nc-settings-gear-65"></i>
@@ -163,7 +346,7 @@
                 </a>
               </li>
             </ul>
-
-            </div>
           </div>
-        </nav>
+        </div>
+      </nav>
+      <!-- End Navbar -->
