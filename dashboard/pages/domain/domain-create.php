7b959fafc9f10afc4685ebf88b6927a2f09b2f6f
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-04 15:15:19 -0400

    commit

--- dashboard/pages/domain/domain-create.php
+++ dashboard/pages/domain/domain-create.php
@@ -1,29 +1,34 @@
 <?php
-// If the SESSION has not been started, start it now
-if (!isset($_SESSION)) {
-    session_start();
-}
 
-// If there is no username, then we need to send them to the login
-//if (!isset($_SESSION['username'])){
-//  header('Location: login.php');
-//}
-
-require('../header.php');
-
-// This function is used to prevent any problems with user form input
-function clean_input($data) {
-  $data = trim($data); //remove spaces at the beginning and end of string
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  $data = str_replace(' ','',$data); //remove any spaces within the string
-  $data = filter_var($data, FILTER_SANITIZE_STRING);
-  return $data;
-}
 
 // We are now going to grab any GET/POST data and put in in SESSION data, then clear it.
 // This will prevent duplicatig actions when page is reloaded.
+if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+  $_SESSION['domain_type'] = "kvm"; //set to "kvm" as this is the only supported type at this time
+  $_SESSION['domain_name'] = clean_input($_POST['domain_name']); //removes spaces and sanitizes
+  $_SESSION['memory_unit'] = $_POST['memory_unit']; //choice of "MiB" or "GiB"
+  $_SESSION['memory'] = $_POST['memory']; //number input, still need to sanitze for number and verify it is not zero
+  $_SESSION['vcpu'] = $_POST['vcpu']; //number input, still need to sanitze for number and verify it is not zero, also may need to set limit to host CPU#
+  $_SESSION['os_arch'] = "x86_64"; //set to x86_64, need to change to host type as well as provide options
+  $_SESSION['os_type'] = "hvm"; //hvm is standard operating system VM
+  $_SESSION['clock_offset'] = "localtime"; //set to localtime
+  $_SESSION['os_platform'] = $_POST['os_platform'];
+  $_SESSION['source_file_volume'] = $_POST['source_file_volume']; //This will be the volume image that the user selects
+  $_SESSION['volume_image_name'] = clean_input($_POST['new_volume_name']); //This is used when a new volume must be created
+  $_SESSION['volume_capacity'] = $_POST['new_volume_size'];
+  $_SESSION['unit'] = $_POST['new_unit'];
+  $_SESSION['volume_size'] = $_POST['new_volume_size'];
+  $_SESSION['driver_type'] = $_POST['new_driver_type'];
+  $_SESSION['source_file_cd'] = $_POST['source_file_cd'];
+  $_SESSION['interface_type'] = $_POST['interface_type'];
+  $_SESSION['mac_address'] = $_POST['mac_address'];
+  $_SESSION['source_dev'] = $_POST['source_dev'];
+  $_SESSION['source_mode'] = $_POST['source_mode'];
+  $_SESSION['source_network'] = $_POST['source_network'];
 
+  header("Location: ".$_SERVER['PHP_SELF']);
+  exit;
+}
 
 if ($_SESSION['domain_type'] == "kvm"]) {
   $domain_type = $_SESSION['domain_type'];
