2367be8a833796199e52d1bb364fccc13798aeec
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-04 15:57:26 -0400

    commit

--- dashboard/pages/domain/domain-create.php
+++ dashboard/pages/domain/domain-create.php
@@ -372,11 +372,13 @@ function changeOptions(selectEl) {
             <div class="tab-pane active" id="general">
 
               <div class="row">
-              <label>Domain Name: </label>
+              <label class="col-sm-2 col-form-label">Domain Name: </label>
+              <div class="col-sm-7">
               <div class="form-group">
                 <input type="text" class="form-control" id="domain_name" required="required" value="newVM" onkeyup="autoDiskName(this.form)" placeholder="Enter a Unique Virtual Machine Name (required)" name="domain_name">
               </div>
             </div>
+          </div>
 
               <div class="row">
                     <label class="col-sm-2 col-form-label">Required Text</label>
@@ -385,9 +387,7 @@ function changeOptions(selectEl) {
                         <input class="form-control" type="text" name="required" required="true" />
                       </div>
                     </div>
-                    <label class="col-sm-3 label-on-right">
-                      <code>required</code>
-                    </label>
+
                   </div>
 
             </div>
