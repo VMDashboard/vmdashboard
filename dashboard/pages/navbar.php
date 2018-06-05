f8fe1d2a1a552a0e62ba4d2b9968434f52148f63
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-05 18:06:33 -0400

    commit

--- dashboard/pages/navbar.php
+++ dashboard/pages/navbar.php
@@ -81,6 +81,35 @@
               </ul>
             </div>
           </li>
+
+          <?php echo (basename($_SERVER['PHP_SELF']) == "storage-pools.php" || basename($_SERVER['PHP_SELF']) == "storage-add-pool.php") ? '<li class="active">' : '<li>'; ?>
+            <a data-toggle="collapse" href="#pagesStorage">
+              <i class="nc-icon nc-book-bookmark"></i>
+              <p>
+                Storage
+                <b class="caret"></b>
+              </p>
+            </a>
+            <div class="collapse <?php echo (basename($_SERVER['PHP_SELF']) == "storage-pools.php" || basename($_SERVER['PHP_SELF']) == "storage-add-pool.php") ? 'show' : ''; ?>" id="pagesStorage">
+              <ul class="nav">
+                <?php echo (basename($_SERVER['PHP_SELF']) == "storage-pools.php") ? '<li class="active">' : '<li>'; ?>
+                  <a href="../storage/storage-pools.php">
+                    <span class="sidebar-mini-icon">SP</span>
+                    <span class="sidebar-normal"> Storage Pools </span>
+                  </a>
+                </li>
+                <?php echo (basename($_SERVER['PHP_SELF']) == "storage-add-pool.php") ? '<li class="active">' : '<li>'; ?>
+                  <a href="../storage/storage-add-pool.php">
+                    <span class="sidebar-mini-icon">CP</span>
+                    <span class="sidebar-normal"> Create New Pool </span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </li>
+
+
+
         </ul>
       </div>
     </div>
