048c9b601b3b8119b021b4f9e0373a9bdbd99117
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-09 16:15:21 -0400

    commit

--- dashboard/pages/network/network-add-lan.php
+++ dashboard/pages/network/network-add-lan.php
@@ -20,9 +20,15 @@ function clean_input($data) {
 
 // We are now going to grab any GET/POST data and put in in SESSION data, then clear it.
 // This will prevent duplicatig actions when page is reloaded.
-if (isset($_POST['pool_name'])) {
-  $_SESSION['pool_name'] = clean_input($_POST['pool_name']);
-  $_SESSION['pool_path'] = $_POST['pool_path'];
+if (isset($_POST['network_name'])) {
+  $_SESSION['network_name'] = clean_input($_POST['network_name']);
+  $_SESSION['forward_mode'] = $_POST['forward_mode'];
+  $_SESSION['mac_address'] = $_POST['mac_address'];
+  $_SESSION['ip_address'] = clean_input($_POST['ip_address']);
+  $_SESSION['subnet_mask'] = $_POST['subnet_mask'];
+  $_SESSION['dhcp_service'] = $_POST['dhcp_service'];
+  $_SESSION['dhcp_start_address'] = clean_input($_POST['dhcp_start_address']);
+  $_SESSION['dhcp_end_address'] = clean_input($_POST['dhcp_end_address']);
 
   header("Location: ".$_SERVER['PHP_SELF']);
   exit;
@@ -30,29 +36,51 @@ if (isset($_POST['pool_name'])) {
 
 require('../header.php');
 
-if (isset($_SESSION['pool_name'])) {
-  $pool_path = $_SESSION['pool_path'];
-  unset($_SESSION['pool_path']);
-  if (substr($pool_path, 0, 16) == "/var/lib/libvirt" || substr($pool_path, 0, 4) == "/mnt" || substr($pool_path, 0, 6) == "/media") {
-    $pool_name = $_SESSION['pool_name'];
-    unset($_SESSION['pool_name']);
+if (isset($_SESSION['network_name'])) {
+  $network_name = $_SESSION['network_name']);
+  $forward_mode = $_SESSION['forward_mode'];
+  $mac_address = $_SESSION['mac_address'];
+  $ip_address = $_SESSION['ip_address'];
+  $subnet_mask = $_SESSION['subnet_mask'];
+  $dhcp_service = $_SESSION['dhcp_service'];
+  $dhcp_start_address = $_SESSION['dhcp_start_address'];
+  $dhcp_end_address = $_SESSION['dhcp_end_address'];
+  unset($_SESSION['network_name']);
+  unset($_SESSION['forward_mode']);
+  unset($_SESSION['mac_address']);
+  unset($_SESSION['ip_address']);
+  unset($_SESSION['subnet_mask']);
+  unset($_SESSION['dhcp_service']);
+  unset($_SESSION['dhcp_start_address']);
+  unset($_SESSION['dhcp_end_address']);
+
+  $xml = "
+  <network>
+    <name>$network_name</name>
+    <forward mode='$forward_mode'/>
+    <mac address='$mac_address'/>
+    <ip address='$ip_address' netmask='$subnet_mask'>
+      <dhcp>
+        <range start='$dhcp_start_address' end='$dhcp_end_address'/>
+      </dhcp>
+    </ip>
+  </network>";
+
+  if ($dhcp_service == "disabled"){
     $xml = "
-      <pool type='dir'>
-        <name>$pool_name</name>
-        <target>
-          <path>$pool_path</path>
-          <permissions>
-          </permissions>
-        </target>
-      </pool>";
+    <network>
+      <name>$network_name</name>
+      <forward mode='$forward_mode'/>
+      <mac address='$mac_address'/>
+      <ip address='$ip_address' netmask='$subnet_mask'>
+      </ip>
+    </network>";
+  }
 
-    $ret = $lv->storagepool_define_xml($xml) ? "success" : "Cannot add storagepool: ".$lv->get_last_error();
-
-    header('Location: storage-pools.php');
+  $ret = $lv->network_define_xml($xml)? 'success' : 'Error while creating network: '.$lv->get_last_error();
+  if ($ret == 'success') {
+    header('Location: network-list.php');
     exit;
-
-  } else {
-    unset($_SESSION['pool_name']);
   }
 
 } //end if $_SESSION
@@ -61,11 +89,28 @@ require('../navbar.php');
 
 ?>
 
+<script>
+function dhcpChangeOptions(selectEl) {
+  let selectedValue = selectEl.options[selectEl.selectedIndex].value;
+    if (selectedValue.charAt(0) === "/") {
+      selectedValue = "enabled";
+    }
+  let subForms = document.getElementsByClassName('dhcpChange')
+  for (let i = 0; i < subForms.length; i += 1) {
+    if (selectedValue === subForms[i].id) {
+      subForms[i].setAttribute('style', 'display:block')
+    } else {
+      subForms[i].setAttribute('style', 'display:none')
+    }
+  }
+}
+</script>
+
 <div class="content">
   <div class="card">
     <form action="" method="POST">
       <div class="card-header">
-        <h4 class="card-title"> Add storage pool to <?php echo $hn; ?></h4>
+        <h4 class="card-title"> Add virtual network to <?php echo $hn; ?></h4>
       </div>
       <div class="card-body">
         <div class="row">
@@ -74,7 +119,7 @@ require('../navbar.php');
               <div class="nav-tabs-wrapper">
                 <ul class="nav nav-tabs flex-column nav-stacked" role="tablist">
                   <li class="nav-item">
-                    <a class="nav-link active" href="#storagePool" role="tab" data-toggle="tab">Storage Pool</a>
+                    <a class="nav-link active" href="#networkLAN" role="tab" data-toggle="tab">Private Network</a>
                   </li>
                 </ul>
               </div>
@@ -84,26 +129,94 @@ require('../navbar.php');
           <div class="col-lg-8 col-md-7 col-sm-8 col-6">
             <!-- Tab panes -->
             <div class="tab-content">
-              <div class="tab-pane active" id="storagePool">
+              <div class="tab-pane active" id="networkLAN">
 
                 <div class="row">
-                  <label class="col-sm-2 col-form-label">Pool Name: </label>
+                  <label class="col-sm-2 col-form-label">Network Name: </label>
                   <div class="col-sm-7">
                     <div class="form-group">
-                      <input type="text" value="default" required="required" placeholder="Enter name for storage pool" class="form-control" name="pool_name" />
+                      <input type="text" value="newNETWORK" required="required" placeholder="Enter name for new network connection " class="form-control" name="network_name" />
                     </div>
                   </div>
                 </div>
 
                 <div class="row">
-                  <label class="col-sm-2 col-form-label">Pool Path: </label>
+                  <label class="col-sm-2 col-form-label">Forward Mode: </label>
                   <div class="col-sm-7">
                     <div class="form-group">
-                      <input type="text" value="/var/lib/libvirt/images" required="required" placeholder="Enter full filepath" class="form-control" name="pool_path" />
-                      <br /> * Only paths that start with <em>/var/lib/libvirt</em>, <em>/media</em>, or <em>/mnt</em> will be allowed
+                      <select class="form-control" name="forward_mode">
+                        <option value="nat" selected>nat</option>
+                      </select>
                     </div>
                   </div>
                 </div>
+
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">MAC Address: </label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <input type="text" class="form-control" name="mac_address" value="<?php echo $random_mac; ?>">
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">Gateway IP Address: </label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <input type="text" class="form-control" name="ip_address" value="192.168.1.1" />
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">Subnet Mask: </label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <select class="form-control" name="subnet_mask">
+                        <option value="255.255.255.0" selected>/24 --> 255.255.255.0</option>
+                        <option value="255.255.255.128">/25  -->  255.255.255.128</option>
+                        <option value="255.255.255.128">/26  -->  255.255.255.192</option>
+                        <option value="255.255.255.128">/27  -->  255.255.255.224</option>
+                        <option value="255.255.255.128">/28  -->  255.255.255.240</option>
+                        <option value="255.255.255.128">/29  -->  255.255.255.248</option>
+                        <option value="255.255.255.128">/30  -->  255.255.255.252</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">DHCP Service: </label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <select class="form-control" name="dhcp_service" onchange="dhcpChangeOptions(this)">
+                        <option value="enabled" selected>enabled</option>
+                        <option value="disabled">disabled</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">DHCP Starting Address: </label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <input type="text" class="form-control" name="dhcp_start_address" value="192.168.1.2" placeholder="Enter starting IP address or none"/>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                  <label class="col-sm-2 col-form-label">DHCP Ending Address: </label>
+                  <div class="col-sm-7">
+                    <div class="form-group">
+                      <input type="text" class="form-control" name="dhcp_end_address" value="192.168.1.254" placeholder="Enter ending IP address or none"/>
+                    </div>
+                  </div>
+                </div>
+
+
 
               </div> <!-- end tab pane -->
             </div> <!-- end tab content -->
