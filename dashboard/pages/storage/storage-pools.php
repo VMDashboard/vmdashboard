b4b197c7467292191057a60726e2269a5bb03525
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-09 22:39:57 -0400

    commit

--- dashboard/pages/storage/storage-pools.php
+++ dashboard/pages/storage/storage-pools.php
@@ -64,7 +64,7 @@ unset($_SESSION['pool']);
 function volumeDeleteWarning(linkURL, fileName) {
   swal({
     title: 'Are you sure?',
-    text: "This will delete the storage volume filename",
+    text: 'This will delete the storage volume',
     type: 'warning',
     confirmButtonText: 'Yes, delete it!',
     showCancelButton: true
