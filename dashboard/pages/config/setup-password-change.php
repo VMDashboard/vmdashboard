1d1ec7c07aab020b77afdb1275ff2a199c60aa24
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-10 13:53:06 -0400

    commit

--- dashboard/pages/config/setup-password-change.php
+++ dashboard/pages/config/setup-password-change.php
@@ -3,10 +3,12 @@
 if (!isset($_SESSION)) {
     session_start();
 }
+
 // If there is no username, then we need to send them to the login
 if (!isset($_SESSION['username'])){
   header('Location: ../login.php');
 }
+
 // We are now going to grab any POST data and put in in SESSION data, then clear it.
 // This will prevent and reloading the webpage to resubmit and action.
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -20,9 +22,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $_SESSION['reset_status'] = "Password was not changed!";
   }
 }
+
 // Time to bring in the header
 require('../header.php');
-
 
 if ($_SESSION['action'] == 'Change') {
   $username = $_SESSION['username'];
@@ -33,27 +35,25 @@ if ($_SESSION['action'] == 'Change') {
 
   require('config.php');
 
-    // Adding the user
-    $sql = "UPDATE openvm_users SET password='$hash' WHERE username='$username'";
-      // Executing the SQL statement
-      if ($conn->query($sql) === TRUE) {
-        //Unset the SESSION variables
-        unset($_SESSION['password']);
-        unset($_SESSION['action']);
-        $_SESSION['reset_status'] = "Password Change Successful!";
-      }
-      $conn->close();
-
+  // Adding the user
+  $sql = "UPDATE openvm_users SET password='$hash' WHERE username='$username'";
+    // Executing the SQL statement
+    if ($conn->query($sql) === TRUE) {
+      //Unset the SESSION variables
+      unset($_SESSION['password']);
+      unset($_SESSION['action']);
+      $_SESSION['reset_status'] = "Password Change Successful!";
+    }
+  $conn->close();
 }
-
 
 if (isset($_SESSION['reset_status'])) {
   $ret = $_SESSION['reset_status'];
   echo "
-  <script>
-  var alert_msg = '$ret'
-  swal(alert_msg);
-  </script>";
+    <script>
+    var alert_msg = '$ret'
+    swal(alert_msg);
+    </script>";
 
   unset($_SESSION['reset_status']);
   if ($ret == "Password Change Successful!") {
@@ -68,34 +68,33 @@ require('../navbar.php');
 <script>
 function checkPassword()
 {
-    //Store the password field objects into variables ...
-    var pass1 = document.getElementById('pass1');
-    var pass2 = document.getElementById('pass2');
-    //Store the Confimation Message Object ...
-    var message = document.getElementById('confirmMessage');
-    //Set the colors we will be using ...
-    var goodColor = "#66cc66";
-    var badColor = "#ff6666";
-    //Compare the values in the password field
-    //and the confirmation field
-    if(pass1.value == pass2.value){
-        //The passwords match.
-        //Set the color to the good color and inform
-        //the user that they have entered the correct password
-        pass2.style.backgroundColor = "#ffffff";
-        message.style.color = goodColor;
-        message.innerHTML = "<p>Passwords Match!</p>"
-    }else{
-        //The passwords do not match.
-        //Set the color to the bad color and
-        //notify the user.
-        pass2.style.backgroundColor = badColor;
-        message.style.color = badColor;
-        message.innerHTML = "<p>Passwords Do Not Match!</p>"
-    }
+  //Store the password field objects into variables ...
+  var pass1 = document.getElementById('pass1');
+  var pass2 = document.getElementById('pass2');
+  //Store the Confimation Message Object ...
+  var message = document.getElementById('confirmMessage');
+  //Set the colors we will be using ...
+  var goodColor = "#66cc66";
+  var badColor = "#ff6666";
+  //Compare the values in the password field
+  //and the confirmation field
+  if(pass1.value == pass2.value){
+    //The passwords match.
+    //Set the color to the good color and inform
+    //the user that they have entered the correct password
+    pass2.style.backgroundColor = "#ffffff";
+    message.style.color = goodColor;
+    message.innerHTML = "<p>Passwords Match!</p>"
+  }else{
+    //The passwords do not match.
+    //Set the color to the bad color and
+    //notify the user.
+    pass2.style.backgroundColor = badColor;
+    message.style.color = badColor;
+    message.innerHTML = "<p>Passwords Do Not Match!</p>"
+  }
 }
 </script>
-
 
 
 <div class="content">
