e5f5e6b0f220c7bab739bb6ddaeab13cc50736fa
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-09 22:40:46 -0400

    commit

--- dashboard/pages/storage/storage-pools.php
+++ dashboard/pages/storage/storage-pools.php
@@ -61,7 +61,7 @@ unset($_SESSION['pool']);
 ?>
 
 <script>
-function volumeDeleteWarning(linkURL, fileName) {
+function volumeDeleteWarning(linkURL) {
   swal({
     title: 'Are you sure?',
     text: 'This will delete the storage volume',
@@ -148,7 +148,7 @@ function volumeDeleteWarning(linkURL, fileName) {
                 "<td>$capacity</td>" .
                 "<td>{$lv->format_size($tmp[$tmp_keys[$ii]]['allocation'], 2)}</td>" .
                "<td>{$tmp[$tmp_keys[$ii]]['path']}</td>" .
-               "<td><a onclick=\"volumeDeleteWarning('?action=volume-delete&amp;path=$path', $filename)\" href=\"#\">Delete</a></td>" .
+               "<td><a onclick=\"volumeDeleteWarning('?action=volume-delete&amp;path=$path')\" href=\"#\">Delete</a></td>" .
                //"<td><a href=\"?action=volume-delete&amp;path=$path\" >Delete</a></td>" .
                "</tr>";
              }
