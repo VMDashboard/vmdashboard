1e54a2556b691b46bd57285c7469466aebf7adc2
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-10 00:26:40 -0400

    commit

--- dashboard/pages/login.php
+++ dashboard/pages/login.php
@@ -22,27 +22,8 @@
   <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
     <div class="container">
       <div class="navbar-wrapper">
-        <div class="navbar-toggle">
-          <button type="button" class="navbar-toggler">
-            <span class="navbar-toggler-bar bar1"></span>
-            <span class="navbar-toggler-bar bar2"></span>
-            <span class="navbar-toggler-bar bar3"></span>
-          </button>
-        </div>
-        <a class="navbar-brand" href="#pablo">Paper Dashboard 2 PRO</a>
-      </div>
-      <div class="logo">
-        <a href="#" class="simple-text logo-mini">
-          <div class="logo-image-small">
-            <img src="../../assets/img/squarelogo.png">
-          </div>
-        </a>
-        <a href="#" class="simple-text logo-normal">
-          OPENVM.TECH
-          <!--<div class="logo-image-big">
-            <img src="../../assets/img/logo.png">
-          </div> -->
-        </a>
+        <img src="../assets/img/squarelogo.png" width="25px">
+        <a class="navbar-brand" href="#">OPENVM.TECH</a>
       </div>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-bar navbar-kebab"></span>
