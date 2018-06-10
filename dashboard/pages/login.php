c9aeb9185e6eafcb0bb1141df2e9bb8c1a5272b3
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-10 00:20:39 -0400

    commit

--- dashboard/pages/login.php
+++ dashboard/pages/login.php
@@ -18,56 +18,7 @@
   </head>
 
 <body class="login-page">
-  <!-- Navbar -->
-  <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
-    <div class="container">
-      <div class="navbar-wrapper">
-        <div class="navbar-toggle">
-          <button type="button" class="navbar-toggler">
-            <span class="navbar-toggler-bar bar1"></span>
-            <span class="navbar-toggler-bar bar2"></span>
-            <span class="navbar-toggler-bar bar3"></span>
-          </button>
-        </div>
-        <a class="navbar-brand" href="#pablo">Paper Dashboard 2 PRO</a>
-      </div>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-bar navbar-kebab"></span>
-        <span class="navbar-toggler-bar navbar-kebab"></span>
-        <span class="navbar-toggler-bar navbar-kebab"></span>
-      </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navigation">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a href="../dashboard.html" class="nav-link">
-              <i class="nc-icon nc-layout-11"></i> Dashboard
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a href="register.html" class="nav-link">
-              <i class="nc-icon nc-book-bookmark"></i> Register
-            </a>
-          </li>
-          <li class="nav-item  active ">
-            <a href="login.html" class="nav-link">
-              <i class="nc-icon nc-tap-01"></i> Login
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a href="user.html" class="nav-link">
-              <i class="nc-icon nc-satisfied"></i> User
-            </a>
-          </li>
-          <li class="nav-item ">
-            <a href="lock.html" class="nav-link">
-              <i class="nc-icon nc-key-25"></i> Lock
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <!-- End Navbar -->
+  
   <div class="wrapper wrapper-full-page ">
     <div class="full-page section-image" filter-color="black" data-image="../../assets/img/bg/fabio-mangione.jpg">
       <!--   you can change the color of the filter page using: data-color="blue | purple | green | orange | red | rose " -->
