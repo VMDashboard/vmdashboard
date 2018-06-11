8c1db56ff58829980d4d1bf8ef31574f9c57ffcb
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-11 10:23:30 -0400

    commit

--- dashboard/pages/domain/domain-single.php
+++ dashboard/pages/domain/domain-single.php
@@ -576,7 +576,11 @@ function domainDeleteWarning(linkURL, domName) {
           ?>
         </div>
       </div>
+    </div> <!-- end row -->
+  </div> <!-- end card -->
 
+  <div class="card">
+    <div class="row">
       <div class="col-md-6">
         <div class="card-header">
           <h6 class="card-title"> Domain XML <?php if ($state != "shutoff"){ echo "(Read Only)"; } ?></h6>
