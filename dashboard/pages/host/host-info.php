78bb8bdf1a0f79861a232b865e117b7dd4f732a6
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-11 06:12:03 -0400

    commit

--- dashboard/pages/host/host-info.php
+++ dashboard/pages/host/host-info.php
@@ -274,10 +274,111 @@ swal(alert_msg);
 
 
             <div class="tab-pane" id="pci">
+              <?php
+              for ($i = 0; $i < sizeof($tmp); $i++) {
+
+                //Just pull out PCI data
+                if ($tmp[$i] == "pci"){
+                  $tmp1 = $lv->get_node_devices($tmp[$i]);
+                  echo "<div class='table-responsive'>" .
+                    "<table class='table'>" .
+                    "<tr>" .
+                    "<th> Device name </th>" .
+                    "<th> Identification </th>" .
+                    "<th> Driver name </th>" .
+                    "<th> Vendor </th>" .
+                    "<th> Product </th>" .
+                    "<th> Action </th>" .
+                    "</tr>";
+
+                  for ($ii = 0; $ii < sizeof($tmp1); $ii++) {
+                    $tmp2 = $lv->get_node_device_information($tmp1[$ii]);
+                    //Actions will be a form button that will submit info using POST
+                    $act = "<form method=\"post\" action=\"\">
+                      <input type=\"hidden\" name=\"action\" value=\"dumpxml\">
+                      <input type=\"hidden\" name=\"name\" value=\"{$tmp2['name']}\">
+                      <input type=\"submit\" name=\"submit\" value=\"XML\">
+                      </form>";
+                    $driver  = array_key_exists('driver_name', $tmp2) ? $tmp2['driver_name'] : 'None';
+                    $vendor  = array_key_exists('vendor_name', $tmp2) ? $tmp2['vendor_name'] : 'Unknown';
+                    $product = array_key_exists('product_name', $tmp2) ? $tmp2['product_name'] : 'Unknown';
+
+                    if (array_key_exists('vendor_id', $tmp2) && array_key_exists('product_id', $tmp2))
+                      $ident = $tmp2['vendor_id'].':'.$tmp2['product_id'];
+                    else
+                      $ident = '-';
+
+                    echo "<tr>" .
+                      "<td>{$tmp2['name']}</td>" .
+                      "<td>$ident</td>" .
+                      "<td>$driver</td>" .
+                      "<td>$vendor</td>" .
+                      "<td>$product</td>" .
+                      "<td>$act</td>" .
+                      "</tr>";
+
+                  }
+
+                  echo "</table></div>";
+
+                }
+              }
+
+              ?>
 
             </div>
 
             <div class="tab-pane" id="usb">
+              <?php
+              for ($i = 0; $i < sizeof($tmp); $i++) {
+
+                //Just pull out USB, USB_DEVICE data
+                if ($tmp[$i] == "usb_device"){
+                  $tmp1 = $lv->get_node_devices($tmp[$i]);
+                  echo "<div class='table-responsive'>" .
+                    "<table class='table'>" .
+                    "<tr>" .
+                    "<th> Device name </th>" .
+                    "<th> Identification </th>" .
+                    "<th> Driver name </th>" .
+                    "<th> Vendor </th>" .
+                    "<th> Product </th>" .
+                    "<th> Action </th>" .
+                    "</tr>";
+
+                  for ($ii = 0; $ii < sizeof($tmp1); $ii++) {
+                    $tmp2 = $lv->get_node_device_information($tmp1[$ii]);
+                    //Actions will be a form button that will submit info using POST
+                    $act = "<form method=\"post\" action=\"\">
+                      <input type=\"hidden\" name=\"action\" value=\"dumpxml\">
+                      <input type=\"hidden\" name=\"name\" value=\"{$tmp2['name']}\">
+                      <input type=\"submit\" name=\"submit\" value=\"XML\">
+                      </form>";
+                    $driver  = array_key_exists('driver_name', $tmp2) ? $tmp2['driver_name'] : 'None';
+                    $vendor  = array_key_exists('vendor_name', $tmp2) ? $tmp2['vendor_name'] : 'Unknown';
+                    $product = array_key_exists('product_name', $tmp2) ? $tmp2['product_name'] : 'Unknown';
+
+                    if (array_key_exists('vendor_id', $tmp2) && array_key_exists('product_id', $tmp2))
+                      $ident = $tmp2['vendor_id'].':'.$tmp2['product_id'];
+                    else
+                      $ident = '-';
+
+                    echo "<tr>" .
+                      "<td>{$tmp2['name']}</td>" .
+                      "<td>$ident</td>" .
+                      "<td>$driver</td>" .
+                      "<td>$vendor</td>" .
+                      "<td>$product</td>" .
+                      "<td>$act</td>" .
+                      "</tr>";
+
+                  }
+
+                  echo "</table></div>";
+
+                }
+              }
+              ?>
 
             </div>
 
