d401810389df52dcdb4eaece0f4ebdfcfd51ec3f
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-11 14:00:37 -0400

    commit

--- dashboard/pages/domain/domain-single2.php
+++ dashboard/pages/domain/domain-single2.php
@@ -239,7 +239,7 @@ function domainDeleteWarning(linkURL, domName) {
 
             <div class="tab-pane active" id="general">
               <div class="row">
-                <div class="col-md-6">
+                <div class="col-md-4">
                   <div class="card-body">
                     <?php
                     if ($state == "running") {
@@ -261,9 +261,32 @@ function domainDeleteWarning(linkURL, domName) {
                   </div>
                 </div>
 
+                <div class="col-md-4">
+                  <div class="card-header">
+                    <h6 class="card-title"> General Information</h6>
+                  </div>
+                  <div class="card-body">
+                    <?php
+                    /* General information */
+                        echo "<strong>Type: </strong>" . $lv->get_domain_type($domName) . "<br />";
+                        echo "<strong>Emulator: </strong>" . $lv->get_domain_emulator($domName) . "<br />";
+                        echo "<strong>Memory: </strong>" . $mem . "<br />";
+                        echo "<strong>vCPUs: </strong>" . $cpu . "<br />";
+                        echo "<strong>State: </strong>" . $state . "<br />";
+                        echo "<strong>Architecture: </strong>" . $arch . "<br />";
+                        echo "<strong>ID: </strong>" . $id . "<br />";
+                        echo "<strong>VNC Port: </strong>" . $vnc . "<br />";
 
+                    if ($die)
+                      die('</body></html');
+                    ?>
+                  </div>
+                </div>
 
-                <div class="col-md-6">
+                <div class="col-md-4">
+                  <div class="card-header">
+                    <h6 class="card-title"> Actions </h6>
+                  </div>
                   <div class="card-body">
                     <ul class="list-unstyled project_files">
                     <?php  if ($state == "running") { ?>
