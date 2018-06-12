e32ab67668dca23b2d93444a436dbc3774582ca4
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-11 22:14:31 -0400

    commit

--- dashboard/pages/domain/domain-single.php
+++ dashboard/pages/domain/domain-single.php
@@ -279,7 +279,7 @@ function domainDeleteWarning(linkURL, domName) {
                         echo "<strong>Architecture: </strong>" . $arch . "<br />";
                         echo "<strong>ID: </strong>" . $id . "<br />";
                         echo "<strong>VNC Port: </strong>" . $vnc . "<br />";
-                        echo "<strong>AutoStart: </strong>" . $lv->domain_get_autostart($dom) . "<br />";
+                        echo "<strong>AutoStart: </strong>" . $lv->domain_get_autostart($domName) . "<br />";
 
                     if ($die)
                       die('</body></html');
