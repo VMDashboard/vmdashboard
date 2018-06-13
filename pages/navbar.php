37cd4664043dce5736f55bc3682ddfc046480d99
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-12 22:32:15 -0400

    commit

--- pages/navbar.php
+++ pages/navbar.php
@@ -172,9 +172,6 @@
                 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                   <i class="nc-icon nc-bell-55"></i>
                   <span class="notification">5</span>
-                  <p>
-                    <span class="d-lg-none d-md-block">Some Actions</span>
-                  </p>
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
                   <a class="dropdown-item" href="#">Action</a>
