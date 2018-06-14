f8d598de1f15dd311948a88ee5f826113edc1b09
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-14 09:35:53 -0400

    commit

--- pages/config/test.php
+++ pages/config/test.php
@@ -37,8 +37,8 @@ require('../navbar.php');
       $percentage = number_format($percentage, 2, '.', ',' );
       echo "CPU Percentage = " . $percentage . "<br><br>";
       ?>
-      <script>
-      ctx = document.getElementById('chartDonut1').getContext("2d");
+<script>
+ctx = document.getElementById('chartDonut1').getContext("2d");
 
 myChart = new Chart(ctx, {
   type: 'pie',
@@ -133,6 +133,71 @@ myChart = new Chart(ctx, {
     </form>
   </div> <!-- end card -->
 </div> <!-- end content -->
+
+<script>
+ctx = document.getElementById('chartDonut1').getContext("2d");
+
+myChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: [1, 2],
+    datasets: [{
+      label: "Emails",
+      pointRadius: 0,
+      pointHoverRadius: 0,
+      backgroundColor: ['#4acccd', '#f4f3ef'],
+      borderWidth: 0,
+      data: [60, 40]
+    }]
+  },
+  options: {
+    elements: {
+      center: {
+        text: '60%',
+        color: '#66615c', // Default is #000000
+        fontStyle: 'Arial', // Default is Arial
+        sidePadding: 60 // Defualt is 20 (as a percentage)
+      }
+    },
+    cutoutPercentage: 90,
+    legend: {
+
+      display: false
+    },
+
+    tooltips: {
+      enabled: false
+    },
+
+    scales: {
+      yAxes: [{
+
+        ticks: {
+          display: false
+        },
+        gridLines: {
+          drawBorder: false,
+          zeroLineColor: "transparent",
+          color: 'rgba(255,255,255,0.05)'
+        }
+
+      }],
+
+      xAxes: [{
+        barPercentage: 1.6,
+        gridLines: {
+          drawBorder: false,
+          color: 'rgba(255,255,255,0.1)',
+          zeroLineColor: "transparent"
+        },
+        ticks: {
+          display: false,
+        }
+      }]
+    },
+  }
+});
+</script>
 
 <?php
 require('../footer.php');
