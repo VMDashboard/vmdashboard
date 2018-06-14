09a4e840b884b9dea219376c71b9ebdbd34bba17
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-14 09:33:55 -0400

    commit

--- pages/config/test.php
+++ pages/config/test.php
@@ -33,8 +33,74 @@ require('../navbar.php');
       $usage0 = $cpu['0']['kernel'] + $cpu['0']['user'];
       $usage1 = $cpu['1']['kernel'] + $cpu['1']['user'];
       $percentage = ($usage1 - $usage0) / ($processor_speed * $multiplier) * 100;
+      // PHP: string number_format ( float $number [, int $decimals [, string $dec_point, string $thousands_sep]] )
+      $percentage = number_format($percentage, 2, '.', ',' );
       echo "CPU Percentage = " . $percentage . "<br><br>";
       ?>
+      <script>
+      ctx = document.getElementById('chartDonut1').getContext("2d");
+
+myChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: [1, 2],
+    datasets: [{
+      label: "Emails",
+      pointRadius: 0,
+      pointHoverRadius: 0,
+      backgroundColor: ['#4acccd', '#f4f3ef'],
+      borderWidth: 0,
+      data: [60, 40]
+    }]
+  },
+  options: {
+    elements: {
+      center: {
+        text: '60%',
+        color: '#66615c', // Default is #000000
+        fontStyle: 'Arial', // Default is Arial
+        sidePadding: 60 // Defualt is 20 (as a percentage)
+      }
+    },
+    cutoutPercentage: 90,
+    legend: {
+
+      display: false
+    },
+
+    tooltips: {
+      enabled: false
+    },
+
+    scales: {
+      yAxes: [{
+
+        ticks: {
+          display: false
+        },
+        gridLines: {
+          drawBorder: false,
+          zeroLineColor: "transparent",
+          color: 'rgba(255,255,255,0.05)'
+        }
+
+      }],
+
+      xAxes: [{
+        barPercentage: 1.6,
+        gridLines: {
+          drawBorder: false,
+          color: 'rgba(255,255,255,0.1)',
+          zeroLineColor: "transparent"
+        },
+        ticks: {
+          display: false,
+        }
+      }]
+    },
+  }
+});
+</script>
 
       <div class="row">
         <div class="col-md-3">
