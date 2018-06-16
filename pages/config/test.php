26ee4984292e7788f41d0aa4b2edf104325f6e4e
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-16 00:16:50 -0400

    commit

--- pages/config/test.php
+++ pages/config/test.php
@@ -104,15 +104,15 @@ $cpu_percentage = number_format($cpu_percentage, 2, '.', ',' ); // PHP: string n
 
             <div class="tab-pane active" id="general">
 
-              <strong>CPU Percentage:</strong> <?php echo $cpu_percentage . "%"; ?> <br /> <br />
+              <strong>CPU Percentage:</strong> <?php echo $cpu_percentage . "%"; ?> <br />
               <div class="progress">
                 <div class="progress-bar progress-bar-danger" role="progressbar" style="width: <?php echo $cpu_percentage . '%'; ?>" aria-valuenow="$mem_used" aria-valuemin="0" aria-valuemax="100"></div>
               </div> <br /> <br />
 
-              <strong>Memory Percentage:</strong> <?php echo $mem_percentage . "%"; ?> <br /> <br />
+              <strong>Memory Percentage:</strong> <?php echo $mem_percentage . "%"; ?> <br />
               <div class="progress">
                 <div class="progress-bar progress-bar-danger" role="progressbar" style="width: <?php echo $mem_percentage . '%'; ?>" aria-valuenow="$mem_used" aria-valuemin="0" aria-valuemax="100"></div>
-              </div> <br /> <br />
+              </div> <br />
 
 
 
