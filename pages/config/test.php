7c02fcb0d9b90e2e7c7ce6479965255d3ac1d75f
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-15 21:18:18 -0400

    commit

--- pages/config/test.php
+++ pages/config/test.php
@@ -79,67 +79,68 @@ require('../navbar.php');
 <?php
 require('../footer.php');
 ?>
+
 <script>
 ctx = document.getElementById('chartDonut1').getContext("2d");
 
-myChart = new Chart(ctx, {
-  type: 'pie',
-  data: {
-    labels: [1, 2],
-    datasets: [{
-      label: "Emails",
-      pointRadius: 0,
-      pointHoverRadius: 0,
-      backgroundColor: ['#4acccd', '#f4f3ef'],
-      borderWidth: 0,
-      data: [95, 5]
-    }]
-  },
-  options: {
-    elements: {
-      center: {
-        text: '10%',
-        color: '#66615c', // Default is #000000
-        fontStyle: 'Arial', // Default is Arial
-        sidePadding: 60 // Defualt is 20 (as a percentage)
-      }
-    },
-    cutoutPercentage: 90,
-    legend: {
+    myChart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: [1, 2],
+        datasets: [{
+          label: "Emails",
+          pointRadius: 0,
+          pointHoverRadius: 0,
+          backgroundColor: ['#4acccd', '#f4f3ef'],
+          borderWidth: 0,
+          data: [60, 40]
+        }]
+      },
+      options: {
+        elements: {
+          center: {
+            text: '60%',
+            color: '#66615c', // Default is #000000
+            fontStyle: 'Arial', // Default is Arial
+            sidePadding: 60 // Defualt is 20 (as a percentage)
+          }
+        },
+        cutoutPercentage: 90,
+        legend: {
 
-      display: false
-    },
-
-    tooltips: {
-      enabled: false
-    },
-
-    scales: {
-      yAxes: [{
-
-        ticks: {
           display: false
         },
-        gridLines: {
-          drawBorder: false,
-          zeroLineColor: "transparent",
-          color: 'rgba(255,255,255,0.05)'
-        }
 
-      }],
-
-      xAxes: [{
-        barPercentage: 1.6,
-        gridLines: {
-          drawBorder: false,
-          color: 'rgba(255,255,255,0.1)',
-          zeroLineColor: "transparent"
+        tooltips: {
+          enabled: false
         },
-        ticks: {
-          display: false,
-        }
-      }]
-    },
-  }
-});
+
+        scales: {
+          yAxes: [{
+
+            ticks: {
+              display: false
+            },
+            gridLines: {
+              drawBorder: false,
+              zeroLineColor: "transparent",
+              color: 'rgba(255,255,255,0.05)'
+            }
+
+          }],
+
+          xAxes: [{
+            barPercentage: 1.6,
+            gridLines: {
+              drawBorder: false,
+              color: 'rgba(255,255,255,0.1)',
+              zeroLineColor: "transparent"
+            },
+            ticks: {
+              display: false,
+            }
+          }]
+        },
+      }
+    });
 </script>
