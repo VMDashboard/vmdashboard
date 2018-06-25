f642829db9c99dc3cfc05d5082b15b6c2b5e6000
matthewalanpenning <matthewalanpenning@gmail.com>
2018-06-25 16:28:19 -0400

    commit

--- pages/config/update.php
+++ pages/config/update.php
@@ -82,7 +82,7 @@ swal(alert_msg);
           $changeLog = file('../../CHANGELOG.md');
           $length = count($changeLog);
           for ($i = 1; $i < $length - 1; $i++) {
-            echo "<pre> $changeLog[$i] </pre>";
+            echo "$changeLog[$i]";
           }
           ?>
 
