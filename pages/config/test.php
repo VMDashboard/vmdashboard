a362d126b0fec2d63af9f77d4aa62f3fb8a2d353
matthewalanpenning <matthewalanpenning@gmail.com>
2018-07-02 11:58:05 -0400

    commit

--- pages/config/test.php
+++ pages/config/test.php
@@ -18,12 +18,9 @@ if (isset($_POST['os'])) {
 require('../header.php');
 
 if (isset($_SESSION['os'])) {
-
   $os = $_SESSION['os'];
   unset($_SESSION['os']);
-
   exec("wget -bqc http://releases.ubuntu.com/18.04/ubuntu-18.04-live-server-amd64.iso");
-
 }
 
 require('../navbar.php');
@@ -42,8 +39,38 @@ require('../navbar.php');
       <div class="card-body">
         <div class="row">
           <div class="col-12">
-            <input type="radio" id="ubuntu" name="os" checked /> Ubuntu Server
-            <input type="radio" id="centos" name="os" /> CentOS
+            <input type="radio" id="ubuntu" value="ubuntu" name="os" checked /> Ubuntu Server <br />
+            <input type="radio" id="centos" value="centos" name="os" /> CentOS <br />
+
+            <select  class="form-control" name="pool">
+
+            <?php
+            $pools = $lv->get_storagepools();
+
+            if (empty($pools)) {
+              <option value="none">No Pool Available</option>
+            } else {
+              for ($i = 0; $i < sizeof($pools); $i++) {
+                //get the pool resource to use with refreshing the pool data
+                $res = $lv->get_storagepool_res($pools[$i]);
+                //refreshing the data before displaying because ISOs were not refreshing automatically and also the Available data was not correct after adding volumes
+                $msg = $lv->storagepool_refresh($res) ? "Pool has been refreshed" : "Error refreshing pool: ".$lv->get_last_error();
+                //getting the pool information to display the data in a table
+                $info = $lv->get_storagepool_info($pools[$i]);
+                $poolName = $pools[$i];
+
+                $act = $info['active'] ? 'Active' : 'Inactive';
+                if ($act == "Active")
+                <option value="$info['path']">$pools[$i]</option>
+              } //ends the for loop for each storage pool
+            } ?>
+
+          </select>
+
+
+
+
+
           </div>
         </div>
       </div> <!-- end card body -->
