fee0aa800347731fe83d5722deadf075a4a48fc0
matthewalanpenning <matthewalanpenning@gmail.com>
2018-07-07 23:29:35 -0400

    commit

--- pages/storage/storage-upload-iso.php
+++ pages/storage/storage-upload-iso.php
@@ -42,7 +42,7 @@ if (isset($_SESSION['pool'])) {
   //$unit = $_SESSION['unit'];
   //$driver_type = $_SESSION['driver_type'];
 
-  $ret = exec("whoami");
+  $ret = exec("ls -la");
 
 //$ret = $lv->storagevolume_upload($pool,"/var/www/html/openvm/pages/storage/ubuntu.iso",0,500) ? "ISO has been uploaded successfully" : "Cannot upload iso: ".$lv->get_last_error();
 
