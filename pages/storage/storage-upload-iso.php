4454d57a3d3ad3dbfaba0d02ee1172697b905876
matthewalanpenning <matthewalanpenning@gmail.com>
2018-07-08 09:01:38 -0400

    commit

--- pages/storage/storage-upload-iso.php
+++ pages/storage/storage-upload-iso.php
@@ -96,7 +96,7 @@ swal(alert_msg);
       <div class="card-header">
         <h4 class="card-title"> Upload ISO image</h4>
         <?php echo "<pre>" . $ret . "</pre>"; ?>
-        <?php echo "<pre>" . $tmp . "</pre>"; ?>
+        <?php var_dump($tmp); ?>
       </div>
       <div class="card-body">
         <div class="row">
