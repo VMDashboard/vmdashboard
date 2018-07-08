4348ae6f970a748166c583418ddd633c9d47ae6c
matthewalanpenning <matthewalanpenning@gmail.com>
2018-07-07 23:36:35 -0400

    commit

--- pages/storage/storage-upload-iso.php
+++ pages/storage/storage-upload-iso.php
@@ -42,7 +42,7 @@ if (isset($_SESSION['pool'])) {
   //$unit = $_SESSION['unit'];
   //$driver_type = $_SESSION['driver_type'];
 
-  $ret = shell_exec("virsh list --all");
+  $ret = shell_exec("virsh -c qemu:///system virsh list --all 2>&1");
   $ret = var_dump($ret);
 
 //$ret = $lv->storagevolume_upload($pool,"/var/www/html/openvm/pages/storage/ubuntu.iso",0,500) ? "ISO has been uploaded successfully" : "Cannot upload iso: ".$lv->get_last_error();
