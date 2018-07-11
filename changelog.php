a114993ac4ec7effed9592c1653cd40487732d6c
matthewalanpenning <matthewalanpenning@gmail.com>
2018-07-11 11:57:04 -0400

    commit

--- changelog.php
+++ changelog.php
@@ -3,5 +3,8 @@
 <h4>Changelog</h4>
 ### A record of significant changes within the development of OpenVM Dashboard
 
+[1.0.1] - 11.JUL.2018
+*Change to how version numbers are compared in the pages/login.php file.
+
 [1.0.0] - 11.JUL.2018
 * Official Stable release of the OpenVM dashboard
